a36b61c4609710575733e2cfffbc79c9b311a875
Helmut Burkhardt <Helmut.Burkhardt@cern.ch>
2002-12-06 11:44:57 +0000

    r144: hbu include unistd.h for sleep

--- gxx11c.c
+++ gxx11c.c
@@ -3,6 +3,7 @@
 #define _HPUX_SOURCE
 #include <stdio.h>
 #include <stdlib.h> //hbu for getenv
+#include <sys/unistd.h> //hbu for sleep
 #include <X11/Xlib.h>
 #include <X11/Xutil.h>
 #include <time.h>
