ff7824c8a7327d0053a66ed5affd146cd8d189d4
Frank Schmidt <Frank.Schmidt@cern.ch>
2003-04-25 15:32:13 +0000

    r190: Version # V1.11

--- madxd.h
+++ madxd.h
@@ -824,7 +824,7 @@ char  tmp_key[NAME_L],
 
 char blank[] = "    ";
 char none[] = "none";
-char myversion[] = "MAD-X 1.10";
+char myversion[] = "MAD-X 1.11";
 char one_string[] = "1";
 char* aux_char_pt;               /* for debug purposes */
 char* exx;
