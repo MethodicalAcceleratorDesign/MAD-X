97a89aae532853e3afe903a8ad9cfb3d5b76177c
Frank Schmidt <Frank.Schmidt@cern.ch>
2005-05-24 14:56:36 +0000

    r528: Version number V2.13.12

--- madxd.h
+++ madxd.h
@@ -985,7 +985,7 @@ char tmp_key[NAME_L],
 char var_form[1000];             /* buffer for the user-controlled formats */
 char blank[] = "    ";
 char none[] = "none";
-char myversion[] = "MAD-X 2.13.11";
+char myversion[] = "MAD-X 2.13.12";
 char one_string[] = "1";
 char aptwfile[FNAME_L] = "dummy"; /* IW 02.12.2004 */
 char* aux_char_pt;               /* for debug purposes */
