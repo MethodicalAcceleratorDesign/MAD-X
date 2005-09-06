f43e08c5c9d6c4a152d21da6211ab3de258ad547
Frank Schmidt <Frank.Schmidt@cern.ch>
2005-09-06 21:19:31 +0000

    r559: version number 3.00.01

--- madxd.h
+++ madxd.h
@@ -994,7 +994,7 @@ char tmp_key[NAME_L],
 char var_form[1000];             /* buffer for the user-controlled formats */
 char blank[] = "    ";
 char none[] = "none";
-char myversion[] = "MAD-X 3.00.00";
+char myversion[] = "MAD-X 3.00.01";
 char one_string[] = "1";
 char aptwfile[FNAME_L] = "dummy"; /* IW 02.12.2004 */
 char* aux_char_pt;               /* for debug purposes */
