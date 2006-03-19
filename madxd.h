6d705bb5957f35b6177d0f7c1df84900054f3524
Frank Schmidt <Frank.Schmidt@cern.ch>
2006-03-19 21:57:56 +0000

    r697: modification date

--- madxd.h
+++ madxd.h
@@ -1044,7 +1044,7 @@ char var_form[1000];             /* buffer for the user-controlled formats */
 char blank[] = "    ";
 char none[] = "none";
 char myversion[] = "MAD-X 3.02.04";
-char code_mod_date[] = "Code Modification Date: 17.03.2006";
+char code_mod_date[] = "Code Modification Date: 19.03.2006";
 char one_string[] = "1";
 char aptwfile[FNAME_L] = "dummy"; /* IW 02.12.2004 */
 char* aux_char_pt;               /* for debug purposes */
