809251fd3a3d9e26244b855094aa2a4c81e6e2f6
Valery Kapin <Valery.Kapin@cern.ch>
2006-04-06 11:19:58 +0000

    r743: MAD-X verion # 3.02.13

--- madxd.h
+++ madxd.h
@@ -1051,7 +1051,7 @@ char tmp_key[NAME_L],
 char var_form[1000];             /* buffer for the user-controlled formats */
 char blank[] = "    ";
 char none[] = "none";
-char myversion[] = "MAD-X 3.02.12";
+char myversion[] = "MAD-X 3.02.13";
 char code_mod_date[] = "Code Modification Date: 06.04.2006";
 char one_string[] = "1";
 char aptwfile[FNAME_L] = "dummy"; /* IW 02.12.2004 */
