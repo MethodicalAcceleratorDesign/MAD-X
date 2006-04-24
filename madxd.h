4bbc1a66b1d14b0d1ec16e889ae56bf39adfb4f5
Valery Kapin <Valery.Kapin@cern.ch>
2006-04-24 23:43:32 +0000

    r839: madX-3_02_28

--- madxd.h
+++ madxd.h
@@ -1051,8 +1051,8 @@ char tmp_key[NAME_L],
 char var_form[1000];             /* buffer for the user-controlled formats */
 char blank[] = "    ";
 char none[] = "none";
-char myversion[] = "MAD-X 3.02.27";
-char code_mod_date[] = "Code Modification Date: 24.04.2006";
+char myversion[] = "MAD-X 3.02.28";
+char code_mod_date[] = "Code Modification Date: 25.04.2006";
 char one_string[] = "1";
 char aptwfile[FNAME_L] = "dummy"; /* IW 02.12.2004 */
 char* aux_char_pt;               /* for debug purposes */
