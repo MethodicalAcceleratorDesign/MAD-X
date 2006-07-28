a53be4342c0fea5ac69e9c292e10591c0a522cb5
Stefan Sorge <ssorge@cern.ch>
2006-07-28 10:28:50 +0000

    r988: madX-3_03_14

--- madxd.h
+++ madxd.h
@@ -1100,8 +1100,8 @@ char tmp_key[NAME_L],
 char var_form[1000];             /* buffer for the user-controlled formats */
 char blank[] = "    ";
 char none[] = "none";
-char myversion[] = "MAD-X 3.03.13";
-char code_mod_date[] = "Code Modification Date: 26.07.2006";
+char myversion[] = "MAD-X 3.03.14";
+char code_mod_date[] = "Code Modification Date: 28.07.2006";
 char one_string[] = "1";
 char aptwfile[FNAME_L] = "dummy"; /* IW 02.12.2004 */
 char* aux_char_pt;               /* for debug purposes */
