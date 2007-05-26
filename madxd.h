4fd2fda774624468cd28fe09f0d9561c486025bb
Frank Schmidt <Frank.Schmidt@cern.ch>
2007-05-26 17:01:18 +0000

    r1270: mad-X_3.03.62

--- madxd.h
+++ madxd.h
@@ -1316,8 +1316,8 @@ char tmp_key[NAME_L],
 char var_form[1000];             /* buffer for the user-controlled formats */
 char blank[] = "    ";
 char none[] = "none";
-char myversion[] = "MAD-X 3.03.61";
-char code_mod_date[] = "Code Modification Date: 24.05.2007";
+char myversion[] = "MAD-X 3.03.62";
+char code_mod_date[] = "Code Modification Date: 26.05.2007";
 char one_string[] = "1";
 char aptwfile[FNAME_L] = "dummy"; /* IW 02.12.2004 */
 char* aux_char_pt;               /* for debug purposes */
