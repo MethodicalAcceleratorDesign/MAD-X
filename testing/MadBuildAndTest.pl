e9c35d0b5630a6cdff4ccbc29c2c126fdaba9d48
Jean Luc Nougaret <nougaret@cern.ch>
2008-02-05 15:23:38 +0000

    r1412: get root directory from command line rather hard-coding

--- testing/MadBuildAndTest.pl
+++ testing/MadBuildAndTest.pl
@@ -1,5 +1,13 @@
 #!/usr/bin/perl
-chdir("/user/nougaret/MAD/bin"); # temporary
+
+# retreive directory hosting the Mad program from the command line
+$thisProgramName = $0;
+$_ = $thisProgramName;
+/^([\w\d\-_.\/]+)\/MadBuildAndTest.pl/;
+
+$hostDirectory = $1;
+chdir($hostDirectory);
+
 `./MadBuild.pl`;
 print "build completed\n";
 # at this stage ./MadCvsExtract/madx dir created locally
