ec770ff32ce07cc4ec284a6cfdfa8cf10126434b
Jean Luc Nougaret <nougaret@cern.ch>
2008-03-28 11:04:35 +0000

    r1529: the build-and-test procedure now applies to the last release detected

--- testing/MadBuildAndTest.pl
+++ testing/MadBuildAndTest.pl
@@ -12,17 +12,40 @@ if ($1 ne "") {
 }
 chdir($hostDirectory);
 
-my $runTest = `./MadTrigTest.pl`;
+# should also retreive the latest release tag
+my @return = `./MadTrigTest.pl`; # extracts MAD's CVS repository with no tag
+
+my $runTest;
+my $releaseTag;
+foreach $retString (@return){
+	my $recognized = 0;
+	if ($retString =~ /^trigger[\s\t]*=[\s\t]*([\w\-]+)$/){
+		$runTest = $1;
+		$recognized = 1;
+	}
+	if ($retString =~ /^releaseTag[\s\t]*=[\s\t]*([\w\-\d_]+)$/){
+		$releaseTag = $1;
+		$recognized = 1;
+	}
+	if ($recognized == 0 ) {
+		print "Unknown string $retString returned by ./MadTrigTest.pl\n";
+	}
+}
+
+# print "\$runTest is '$runTest', and \$releaseTag is '$releaseTag'\n";
 
 # at this stage ./MadCvsExtract/madx dir created locally
 # (currently overwritten by MadBuild.pl)
 
 if ($runTest eq "run-test") {
 
+	# the build-and-test procedure always applies to the last release
+	# even if CVS commits took place afterwards. 
+
 	# irrespective of which directory MadTrigTest.pl ended in,
 	# MadBuild.pl starts from the location set by calling MadBuildAndTest.pl program
-
-	`./MadBuild.pl`; # issues e-mail upon completion
+	
+	`./MadBuild.pl $releaseTag`; # issues e-mail upon completion
 	
 	# irrespective of which directory MadBuild.pl ended in,
 	# MadTest.pl starts from the location set by calling MadBuildAndTest.pl program
