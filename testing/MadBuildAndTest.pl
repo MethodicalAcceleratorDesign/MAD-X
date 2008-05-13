11c974e9c2992cb7d2c3794d9752240e9d3040aa
Jean Luc Nougaret <nougaret@cern.ch>
2008-05-13 12:39:16 +0000

    r1588: some messages to follow-up the procedure

--- testing/MadBuildAndTest.pl
+++ testing/MadBuildAndTest.pl
@@ -13,7 +13,9 @@ if ($1 ne "") {
 chdir($hostDirectory);
 
 # should also retreive the latest release tag
+print "entering MadTrigTest.pl\n";
 my @return = `./MadTrigTest.pl`; # extracts MAD's CVS repository with no tag
+print "MadTrigTest.pl completed\n";
 
 my $runTest;
 my $releaseTag;
@@ -45,12 +47,16 @@ if ($runTest eq "run-test") {
 	# irrespective of which directory MadTrigTest.pl ended in,
 	# MadBuild.pl starts from the location set by calling MadBuildAndTest.pl program
 	
+	print "entering MadBuild.pl\n";	
 	`./MadBuild.pl $releaseTag`; # issues e-mail upon completion
+	print "MadBuild.pl completed\n";
 	
 	# irrespective of which directory MadBuild.pl ended in,
 	# MadTest.pl starts from the location set by calling MadBuildAndTest.pl program
 	
+	print "entering MadTest.pl\n";
 	`./MadTest.pl ./MadCvsExtract/madX`; # issues e-mail upon completion
+	print "MadTest.pl completed\n";
 } else {
 	print "No new release detected => no need to run the test-suite\n";
 }
