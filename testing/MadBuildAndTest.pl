90e7627785d672856f7e658f9e66f010bd5e304f
Jean Luc Nougaret <nougaret@cern.ch>
2008-06-10 07:26:11 +0000

    r1652: wait until the test procedure is triggered to fork the process, so as to ensure that the kerberos/AFS log file is not overwritten every day

--- testing/MadBuildAndTest.pl
+++ testing/MadBuildAndTest.pl
@@ -12,8 +12,71 @@ if ($1 ne "") {
 }
 chdir($hostDirectory);
 
+open REPORT_FILE, ">MadBuildAndTest_Report.txt";
+my $now = localtime;
+print REPORT_FILE "MadBuildAndTest.pl report from $now\n";
+	
+my $releaseTag;
+my $runTest;
+my $debugMode;
+	
+my $argsNumber = $#ARGV+1;
+	
+if ($argsNumber>0){
+	$debugMode = 1;
+} else {
+	$debugMode = 0;
+}
+	
 
-my $child_pid = fork();
+
+if ($debugMode==0){
+	# should also retreive the latest release tag
+	print REPORT_FILE "entering MadTrigTest.pl\n";
+	my @return = `./MadTrigTest.pl`; # extracts MAD's CVS repository with no tag
+	print REPORT_FILE "MadTrigTest.pl completed\n";
+
+	foreach $retString (@return){
+		my $recognized = 0;
+		if ($retString =~ /^trigger[\s\t]*=[\s\t]*([\w\-]+)$/){
+			$runTest = $1;
+			$recognized = 1;
+		}
+		if ($retString =~ /^releaseTag[\s\t]*=[\s\t]*([\w\-\d_]+)$/){
+			$releaseTag = $1;
+			$recognized = 1;
+		}
+		if ($recognized == 0 ) {
+			print REPORT_FILE "Unknown string $retString returned by ./MadTrigTest.pl\n";
+		}
+	}
+} else {
+	# expect "debug=madX-x_yy_zz" where madX-x_yy_zz is the release tag
+	$firstArg = $ARGV[0];
+	if ($firstArg =~ /debug=(madX\-\d_\d\d_\d\d)/){
+		$releaseTag = $1;
+		$runTest = 'run-test';
+	} else {
+		print "Expect either no argument (automatic trigger)or argument of form:\n";
+		print "\tdebug=madX-x_yy_zz\n";
+		# no need to kill the child process: it was not yet forked
+		# print "Kill the child process\n";
+		#kill 9, $child_pid; # kill the child process
+		print REPORT_FILE "Exit!\n";
+		exit;
+	}
+}
+
+my $child_pid;
+
+if ($runTest eq "run-test") {
+	$child_pid = fork();
+} else {
+	print REPORT_FILE "No new release detected => no need to run the test-suite\n";
+	close REPORT_FILE;
+	exit;
+}
+
 
 if (not defined $child_pid){
 	print "no system resources to fork process\n";
@@ -58,86 +121,28 @@ if ($child_pid==0){
 if ($child_pid) {
 	# non-zero pid means we are in the parent process, which received the child's pid
 
-	open REPORT_FILE, ">MadBuildAndTest_Report.txt";
-	my $now = localtime;
-	print REPORT_FILE "MadBuildAndTest.pl report from $now\n";
-	
-	my $releaseTag;
-	my $runTest;
-	my $debugMode;
-	
-	my $argsNumber = $#ARGV+1;
-	
-	if ($argsNumber>0){
-		$debugMode = 1;
-	} else {
-		$debugMode = 0;
-	}
-	
-
-
-	if ($debugMode==0){
-		# should also retreive the latest release tag
-		print REPORT_FILE "entering MadTrigTest.pl\n";
-		my @return = `./MadTrigTest.pl`; # extracts MAD's CVS repository with no tag
-		print REPORT_FILE "MadTrigTest.pl completed\n";
-
-		foreach $retString (@return){
-			my $recognized = 0;
-			if ($retString =~ /^trigger[\s\t]*=[\s\t]*([\w\-]+)$/){
-				$runTest = $1;
-				$recognized = 1;
-			}
-			if ($retString =~ /^releaseTag[\s\t]*=[\s\t]*([\w\-\d_]+)$/){
-				$releaseTag = $1;
-				$recognized = 1;
-			}
-			if ($recognized == 0 ) {
-				print REPORT_FILE "Unknown string $retString returned by ./MadTrigTest.pl\n";
-			}
-		}
-	} else {
-		# expect "debug=madX-x_yy_zz" where madX-x_yy_zz is the release tag
-		$firstArg = $ARGV[0];
-		if ($firstArg =~ /debug=(madX\-\d_\d\d_\d\d)/){
-			$releaseTag = $1;
-			$runTest = 'run-test';
-		} else {
-			print "Expect either no argument (automatic trigger)or argument of form:\n";
-			print "\tdebug=madX-x_yy_zz\n";
-			print "Kill the child process\n";
-			kill 9, $child_pid; # kill the child process
-			print REPORT_FILE "Exit!\n";
-			exit;
-		}
-	}
-
 	# print REPORT_FILE "\$runTest is '$runTest', and \$releaseTag is '$releaseTag'\n";
 
 	# at this stage ./MadCvsExtract/madx dir created locally
 	# (currently overwritten by MadBuild.pl)
 
-	if ($runTest eq "run-test") {
 
-		# the build-and-test procedure always applies to the last release
-		# even if CVS commits took place afterwards. 
+	# the build-and-test procedure always applies to the last release
+	# even if CVS commits took place afterwards. 
 
-		# irrespective of which directory MadTrigTest.pl ended in,
-		# MadBuild.pl starts from the location set by calling MadBuildAndTest.pl program
+	# irrespective of which directory MadTrigTest.pl ended in,
+	# MadBuild.pl starts from the location set by calling MadBuildAndTest.pl program
 	
-		print REPORT_FILE "entering MadBuild.pl\n";	
-		`./MadBuild.pl $releaseTag`; # issues e-mail upon completion
-		print REPORT_FILE "MadBuild.pl completed\n";
+	print REPORT_FILE "entering MadBuild.pl\n";	
+	`./MadBuild.pl $releaseTag`; # issues e-mail upon completion
+	print REPORT_FILE "MadBuild.pl completed\n";
 	
-		# irrespective of which directory MadBuild.pl ended in,
-		# MadTest.pl starts from the location set by calling MadBuildAndTest.pl program
+	# irrespective of which directory MadBuild.pl ended in,
+	# MadTest.pl starts from the location set by calling MadBuildAndTest.pl program
 	
-		print REPORT_FILE "entering MadTest.pl\n";
-		`./MadTest.pl ./MadCvsExtract/madX`; # issues e-mail upon completion
-		print REPORT_FILE "MadTest.pl completed\n";
-	} else {
-		print REPORT_FILE "No new release detected => no need to run the test-suite\n";
-	}
+	print REPORT_FILE "entering MadTest.pl\n";
+	`./MadTest.pl ./MadCvsExtract/madX`; # issues e-mail upon completion
+	print REPORT_FILE "MadTest.pl completed\n";
 
 	close REPORT_FILE;
 	
