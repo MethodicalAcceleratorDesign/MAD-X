166de35cb9b9aba261117df852340a106eaa8c8a
Frank Schmidt <Frank.Schmidt@cern.ch>
2008-12-19 00:24:13 +0000

    r1885: madx-3.04.63

--- madxd.h
+++ madxd.h
@@ -1365,8 +1365,8 @@ char tmp_key[NAME_L],
 char var_form[1000];             /* buffer for the user-controlled formats */
 char blank[] = "    ";
 char none[] = "none";
-char myversion[] = "MAD-X 3.04.62";
-char code_mod_date[] = "Code Modification Date: 16.12.2008";
+char myversion[] = "MAD-X 3.04.63";
+char code_mod_date[] = "Code Modification Date: 18.12.2008";
 char one_string[] = "1";
 char aptwfile[FNAME_L] = "dummy"; /* IW 02.12.2004 */
 char* aux_char_pt;               /* for debug purposes */
