4a280c4820915181988d064bbfa3e7fb319ef001
Frank Schmidt <Frank.Schmidt@cern.ch>
2009-02-22 02:41:27 +0000

    r1976: MAD-X vesrion: 4.00.02

--- madxd.h
+++ madxd.h
@@ -1365,8 +1365,8 @@ char tmp_key[NAME_L],
 char var_form[1000];             /* buffer for the user-controlled formats */
 char blank[] = "    ";
 char none[] = "none";
-char myversion[] = "MAD-X 4.00.01";
-char code_mod_date[] = "Code Modification Date: 20.02.2009";
+char myversion[] = "MAD-X 4.00.02";
+char code_mod_date[] = "Code Modification Date: 22.02.2009";
 char one_string[] = "1";
 char aptwfile[FNAME_L] = "dummy"; /* IW 02.12.2004 */
 char* aux_char_pt;               /* for debug purposes */
