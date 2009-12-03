28e3c1bb9e839c61ced6f08da1630529918e5dd3
Jean Luc Nougaret <nougaret@cern.ch>
2009-12-03 15:46:52 +0000

    r2371: work in progress

--- testing/MadBuild.py
+++ testing/MadBuild.py
@@ -3,19 +3,22 @@
 
 import time
 import optparse
+import os
+import re
+import shutil
 
 # MISSING TIME MEASUREMENT
 
-
-
 web = "/afs/cern.ch/user/n/nougaret/www/mad/"
+currentDir = os.getcwd()
+extractDir = currentDir+'/MadCvsExtract'
 
 class Repository:
     repoDir = ":gserver:isscvs.cern.ch:/local/reps/madx"
     def __init__(self):
         pass
-    def checkout(self):
-        command = "cvs -d " + repoDir + " checkout -r" + releaseTag + "madX" 
+    def checkout(self,releaseTag):
+        command = "cvs -d " + Repository.repoDir + " checkout -r" + releaseTag + " madX"
         os.system(command)
 
 # first set environment variables for lf95 and NAG compilers
@@ -25,14 +28,18 @@ class Repository:
 def main():
     usage = "%prog [options]"
     parser = optparse.OptionParser(usage)
-    parser.add_option('--release','-r',help="release tag",action="store_true")
+    parser.add_option('--release','-r',help="release tag",dest="releaseTag")
     parser.add_option('--dev','-d',help="construct executable with Makefile_develop",action="store_true")
     parser.add_option('--nag','-n',help="construct executable with Makefile_nag",action="store_true")
     (options,args) = parser.parse_args()
 
-    if not options.release:
+    if not options.releaseTag:
         raise("except a release tag to be specified")
-    
+    else: # is the release tag well formed?
+        releasePattern = re.compile(r'^madX\-\d+_\d{2}_\d{2}$')
+        if not releasePattern.match(options.releaseTag):
+            raise("release tag is ill-formed: it should be like 'madX-4_01_01' instead of '"+options.releaseTag+"'")
+
     os.environ['PATH'] = os.environ['PATH'] +\
                          ":/afs/cern.ch/sw/fortran/nag/f95.5.361/bin:/afs/cern.ch/sw/fortran/lahey/lf9562/bin"
     os.environ['LD_LIBRARY_PATH'] = os.environ['LD_LIBRARY_PATH'] + ":/afs/cern.ch/sw/fortran/lahey/lf9562/lib"
@@ -47,19 +54,38 @@ def main():
     if options.dev:
         makefiles.append('Makefile_develop')
 
+    # if the directory in which to extract the CVS does not yet exist, then create it
+    if not os.path.exists(extractDir):
+        os.mkdir(extractDir)
+    os.chdir(extractDir)
     repo = Repository()
-    repo.checkout()
-    
+    repo.checkout(options.releaseTag)
+    os.chdir('./madX')
+ 
     for m in makefiles:
-        if m == Makefile:
-            invocation = "Makefile f95=/opt/intel/Compiler/11.0/081/bin/ia32/ifort DEBUG=NO madx"
-        elif m == Makefile_develop:
-            invocation = "Makefile f95=lf95 DEBUG=YES madx" # the Lahey Fortran compiler
-        elif m == Makefile_nag:
-            invocation = "Makefile f95=f95 DEBUG = YES madx" # the NAG compiler
+        # do a make clean and remove any preexisting target
+        if os.path.exists('./madx'):
+            os.remove('./madx')
+        os.system('make clean')
+        
+        print("now to compile "+m+" in " + os.getcwd())
+        if m == 'Makefile':
+            invocation = "make f95=/opt/intel/Compiler/11.0/081/bin/ia32/ifort DEBUG=NO madx"
+        elif m == 'Makefile_develop':
+            invocation = "make f95=lf95 DEBUG=YES madx" # the Lahey Fortran compiler
+        elif m == 'Makefile_nag':
+            invocation = "make f95=f95 DEBUG = YES madx" # the NAG compiler
 
         output = "./makeResult"
+        #os.system(invocation)
         os.system(invocation+">& "+output)
+        # if the target madx is absent, compilation failed
+        if os.path.exists('./madx'):
+            # success or warning
+            shutil.copyfile('./madx','./madx_'+m)
+            print("succesfully compiled for "+m)
+        else: # failure
+            print("failed to compile for "+m)
 
 if __name__ == "__main__":
     main()
