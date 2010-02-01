17aaa2bead7de8b491fd2afe7ba19ff71ea5ee8e
Frank Schmidt <Frank.Schmidt@cern.ch>
2010-02-01 11:15:28 +0000

    r2397: version: MAD-X 4.01.19

--- madxd.h
+++ madxd.h
@@ -1384,10 +1384,10 @@ char tmp_key[NAME_L],
 char var_form[1000];             /* buffer for the user-controlled formats */
 char blank[] = "    ";
 char none[] = "none";
-char myversion[] = "MAD-X 4.01.18";
+char myversion[] = "MAD-X 4.01.19";
 char version_type[] = "DEBUG Version - use at own risk!";
 /* char version_type[] = "       Production Version       "; */
-char code_mod_date[] = "Code Modification Date: 18.01.2010";
+char code_mod_date[] = "Code Modification Date: 01.02.2010";
 char one_string[] = "1";
 char aptwfile[FNAME_L] = "dummy"; /* IW 02.12.2004 */
 char* aux_char_pt;               /* for debug purposes */
