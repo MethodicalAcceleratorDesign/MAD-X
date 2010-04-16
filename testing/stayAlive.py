1389ebeae5a9a4c54e4a13c39cade7f57b640dc1
Jean Luc Nougaret <nougaret@cern.ch>
2010-04-16 15:45:49 +0000

    r2453: prints to stdout instead of notifying by e-mail

--- testing/stayAlive.py
+++ testing/stayAlive.py
@@ -1,47 +1,38 @@
 #!/usr/bin/python
-
 import threading
-from Notify import notify
-import time
 import os
-import sys
-import re
+import time
 
 class updateTokensThread(threading.Thread):
     def __init__(self):
         threading.Thread.__init__(self)
     def run(self):
-        startTime = time.ctime()
         while True:
             try:
-                # provide path to cope with reduced acron environment
                 os.system('/usr/sue/bin/kinit -k -t /extra/home/nougaret/MadTestScriptAuth'+
                           '/mykeytab nougaret')
-                os.system('/usr/bin/aklog')
-                # must provide path to cope with reduced acron environment
-                os.system('/usr/sue/bin/klist > ' + outfile)             
+                os.system('/usr/bin/aklog')           
+                outfile = "stayAlive.out"
+                os.system('/usr/kerberos/bin/klist > ' + outfile)            
                 f = open(outfile,'r')             
                 lines = f.readlines()               
                 msg2 = ''
                 for line in lines:
                     msg2 = msg2 + line + '\n'
                 f.close()              
-                os.system('rm '+outfile)               
+                os.system('rm '+outfile)
+                print('klist contents:' + msg2)
             except:
-                notify('jean-luc','failure','failed to refresh tokens!')
-                for sec in range(1,30): # sleep 30 seconds
-                    time.sleep(1) # 1 sec
-                    if self.kill == True:
-                        return
-            now = time.ctime()
-            notify('jean-luc','alive','still alive after '+ +'hours')
+                print('failed to refresh tokens!')
+            for sec in range(1,30): # sleep 30 seconds
+                time.sleep(1) # 1 sec
+                if self.kill == True:
+                    return
     def kill(self):
         self.kill = True # work-around: not yet able to kill Python threads
 
-print 'now about to launch thread'
 th = updateTokensThread()
 th.start()
-print 'threaded launched'
 
 while True:
     pass
