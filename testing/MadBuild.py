68c0e50a3ada20d9607659659bf766f7b5002ec4
Jean Luc Nougaret <nougaret@cern.ch>
2010-05-04 12:08:47 +0000

    r2463: solve the problem with FORT_FMT_RECL

--- testing/MadBuild.py
+++ testing/MadBuild.py
@@ -60,7 +60,7 @@ def main():
 # launched through os.sytem() calls.
 # all 'madx < input > output' calls where madx is compiled with the ifort
 # Intel compiler will take FORT_FMT_RECL into account.
-    os.environ['FORT_FMT_RECL']='256' # lines split after 256 chars instead of
+#    os.environ['FORT_FMT_RECL']='256' # lines split after 256 chars instead of
 # THIS DOES NOT SEEM TO WORK 13 april 2010 - force the variable when invoking the makefile instead
 # the default 80    
          
@@ -137,13 +137,15 @@ def main():
 
         #print("now to compile "+m+" in " + os.getcwd())
         if m == 'Makefile':
-            invocation = "make f95=/opt/intel/Compiler/11.0/081/bin/ia32/ifort DEBUG=NO FORT_FMT_RECL=256 madx"
+            #invocation = "make f95=/opt/intel/Compiler/11.0/081/bin/ia32/ifort DEBUG=NO FORT_FMT_RECL=256 madx"
+            invocation = "make f95=ifort DEBUG=NO FORT_FMT_RECL=256 madx"
         elif m == 'Makefile_develop':
             invocation = "make f95=lf95 DEBUG=YES madx" # the Lahey Fortran compiler
         elif m == 'Makefile_nag':
             invocation = "make f95=f95 DEBUG = YES madx" # the NAG compiler
         else:
             raise("should never reach this point")
+        print "make invocation=",invocation
         #notify('jean-luc','Point 3.2', 'makefile='+m) 
         output = "./makeResult"
         #os.system(invocation)
