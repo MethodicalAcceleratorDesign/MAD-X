6f3cb2e39f3c3eb440c1eccb3922e59e63dccf6a
Frank Schmidt <Frank.Schmidt@cern.ch>
2011-05-05 14:39:16 +0000

    r2701: MAD-X version: 5.00.04

--- madxd.h
+++ madxd.h
@@ -1391,10 +1391,10 @@ char tmp_key[NAME_L],
 char var_form[1000];             /* buffer for the user-controlled formats */
 char blank[] = "    ";
 char none[] = "none";
-char myversion[] = "MAD-X 5.00.03";
+char myversion[] = "MAD-X 5.00.04";
 char version_type[] =    "DEBUG Version - use at own risk!";
 /* char version_type[] = "       Production Version       ";    */
-char code_mod_date[] = "Code Modification Date: 20.04.2011";
+char code_mod_date[] = "Code Modification Date: 05.05.2011";
 char one_string[] = "1";
 char aptwfile[FNAME_L] = "dummy"; /* IW 02.12.2004 */
 char* aux_char_pt;               /* for debug purposes */
