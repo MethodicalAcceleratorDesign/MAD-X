0e5a10101be57e4f623754c09b0b9e7572088259
Yngve Inntjore Levinsen <yngve.levinsen@gmail.com>
2011-05-15 09:17:15 +0000

    r2716: Reverted cmake globbing  - problem because some sources (e.g. madxe.c) should not be compiled
     this reverts commit 2710,2712

--- cmakesrc/setupSources.cmake
+++ cmakesrc/setupSources.cmake
@@ -1,20 +1,17 @@
 
 # list of c source files
-FILE (GLOB csrcfiles *.c)
+set(csrcfiles madxp.c gxx11c.c matchptcknobs.c rplot.c )
 # list of fortran source files
-FILE (GLOB fsrcfiles *.f90 *.F90)
+set(fsrcfiles madx_main.F90 Sqb_accel_ptc.f90 Sr_spin.f90 Sra_fitting.f90 madx_ptc_intstate.f90 madx_ptc_setcavs.f90 madx_ptc_knobs.f90 madx_ptc_module.f90 madx_ptc_distrib.f90 madx_ptc_eplacement.f90 madx_ptc_normal.f90 madx_ptc_script.f90 madx_ptc_trackcavs.f90 madx_ptc_track_run.f90 madx_ptc_twiss.f90 match.f90 matchjc.f90 matchlib.f90 matchsa.f90 orbf.f90 plot.f90 poisson.f90 ptc_export_xml.f90 resindex.f90 run_madx.f90 sodd.f90 Spb_fake_gino_sub.f90 St_pointers.f90 survey.f90 timest.f90 timex.f90 touschek.f90 trrun.f90 twiss.f90 user2_photon.f90 wrap.f90 fortran_flush.F90 a_scratch_size.f90 b_da_arrays_all.f90 d_lielib.f90 util.f90 dynap.f90 emit.f90 gxx11.f90 h_definition.f90 ibsdb.f90 i_tpsa.f90 j_tpsalie.f90 k_tpsalie_analysis.f90 l_complex_taylor.f90 m_real_polymorph.f90 n_complex_polymorph.f90 o_tree_element.f90 Sa_extend_poly.f90 Sb_sagan_pol_arbitrary.f90 Sc_euclidean.f90 Sd_frame.f90 Se_status.f90 Sf_def_all_kinds.f90 Sg_sagan_wiggler.f90 Sh_def_kind.f90 Si_def_element.f90 Sk_link_list.f90 Sl_family.f90 Sm_tracking.f90 Sma0_beam_beam_ptc.f90 Sma_multiparticle.f90 Sn_mad_like.f90 So_fitting.f90 Sp_keywords.f90 Sq_orbit_ptc.f90)
 
 
 # add source files according to NTPSA option...
 if (MADX_NTPSA )
   message("NTPSA turned on")
-	file(GLOB to_remove c_dabnew.f90)
-	list(REMOVE_ITEM fsrcfiles ${to_remove})
+ set(fsrcfiles ${fsrcfiles} c_dabnew_berz.f90 c_tpsa_interface.F90)
+ set(csrcfiles ${csrcfiles} tpsa.cpp)
 else (MADX_NTPSA )
-	file(GLOB to_remove c_dabnew_berz.f90 c_tpsa_interface.F90)
-	list(REMOVE_ITEM fsrcfiles ${to_remove})
-	file(GLOB to_remove tpsa.cpp)
-	list(REMOVE_ITEM csrcfiles ${to_remove})
+ set(fsrcfiles ${fsrcfiles} c_dabnew.f90)
 endif  (MADX_NTPSA )
 
 #execute python wrapper scripts (you need to be dependent on one of the output files or else this command will never be ran):
@@ -38,8 +35,9 @@ ADD_CUSTOM_COMMAND(
 
 
 # main source files... 
+# TODO: please check this list at some point!
 set(srcfiles ${csrcfiles} ${fsrcfiles} ${CMAKE_CURRENT_BINARY_DIR}/fortran_wrappers.c ${CMAKE_CURRENT_BINARY_DIR}/c_wrappers.c )
 
 # header files...
-file(GLOB headerfiles *.h)
+set(headerfiles c6t.h madxdict.h madxl.h matchptcknobs.h tpsa.h madxd.h madx.h madxreg.h rplot.h)
 
