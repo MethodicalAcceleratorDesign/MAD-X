401666df21942ac035b399e8f18f75148d5f8e9e
Yngve Inntjore Levinsen <yngve.levinsen@gmail.com>
2011-05-17 13:06:22 +0000

    r2718: cmake:  - updated patchlevel  - build name for cdash more useful

--- CTestConfig.cmake
+++ CTestConfig.cmake
@@ -11,3 +11,11 @@ set(CTEST_DROP_METHOD "http")
 set(CTEST_DROP_SITE "137.138.26.237")
 set(CTEST_DROP_LOCATION "/cdash/submit.php?project=Mad-X")
 set(CTEST_DROP_SITE_CDASH TRUE)
+
+
+ADD_TEST(testruns "${CMAKE_CURRENT_SOURCE_DIR}/cmakesrc/ctests/testruns.sh" "${BINARY_POSTFIX}")
+set_tests_properties (testruns 
+  PROPERTIES PASS_REGULAR_EXPRESSION "Number of warnings: 0")
+ADD_TEST(testsample "${CMAKE_CURRENT_SOURCE_DIR}/cmakesrc/ctests/testsample.sh" "${BINARY_POSTFIX}" "${CMAKE_CURRENT_SOURCE_DIR}/cmakesrc/ctests")
+set_tests_properties (testsample 
+  PROPERTIES FAIL_REGULAR_EXPRESSION "Number of errors: [1-9]") # The no warning check fails on this test..
