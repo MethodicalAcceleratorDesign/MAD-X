94ae049a44a2503f11c7ededf8566fd80bdcc803
Yngve Inntjore Levinsen <yngve.levinsen@gmail.com>
2011-05-19 15:17:35 +0000

    r2723: added tests using ctest these are all taken from madX-examples, and depend on afs being available to succeed. use command "ctest" after make to run them

---
+++ cmakesrc/ctests/testbase.cmake
@@ -0,0 +1,14 @@
+execute_process(COMMAND cat ${SOURCEDIR}/${TEST_SCRIPT} COMMAND ${TEST_PROG}
+                RESULT_VARIABLE HAD_ERROR)
+if(HAD_ERROR)
+    message(FATAL_ERROR "Test failed")
+endif()
+
+if(${TEST_OUTPUT})
+    execute_process(COMMAND ${CMAKE_COMMAND} -E compare_files
+        ${TEST_OUTPUT} /afs/cern.ch/user/y/ylevinse/scratch1/public/madx_testing_output/${TEST_OUTPUT}
+        RESULT_VARIABLE DIFFERENT)
+    if(DIFFERENT)
+        message(FATAL_ERROR "Test failed - files differ")
+    endif()
+endif()
