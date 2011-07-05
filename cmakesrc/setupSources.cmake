478b0f59f5052f50c1f98a01185cd0f0dfb27cfc
Yngve Inntjore Levinsen <yngve.levinsen@gmail.com>
2011-07-05 10:51:08 +0000

    r2755: cmake working again - change logic:  if LAPACK/BLAS is found, linking to those and exclude      matchlib/matchlib2 from source array

--- cmakesrc/setupSources.cmake
+++ cmakesrc/setupSources.cmake
@@ -2,12 +2,20 @@
 # list of c source files
 set(csrcfiles madxp.c gxx11c.c matchptcknobs.c rplot.c )
 # list of fortran source files
-set(fsrcfiles Sr_spin.f90 Sra_fitting.f90 madx_ptc_intstate.f90 madx_ptc_setcavs.f90 madx_ptc_knobs.f90 madx_ptc_module.f90 madx_ptc_distrib.f90 madx_ptc_eplacement.f90 madx_ptc_normal.f90 madx_ptc_script.f90 madx_ptc_trackcavs.f90 madx_ptc_track_run.f90 madx_ptc_twiss.f90 match.f90 matchlib2.f90 matchsa.f90 orbf.f90 plot.f90 poisson.f90 ptc_export_xml.f90 resindex.f90 run_madx.f90 sodd.f90 Spb_fake_gino_sub.f90 St_pointers.f90 survey.f90 timest.f90 timex.f90 touschek.f90 trrun.f90 twiss.f90 user2_photon.f90 wrap.f90 fortran_flush.F90 a_scratch_size.f90 b_da_arrays_all.f90 d_lielib.f90 util.f90 dynap.f90 emit.f90 gxx11.f90 h_definition.f90 ibsdb.f90 i_tpsa.f90 j_tpsalie.f90 k_tpsalie_analysis.f90 l_complex_taylor.f90 m_real_polymorph.f90 n_complex_polymorph.f90 o_tree_element.f90 Sa_extend_poly.f90 Sb_sagan_pol_arbitrary.f90 Sc_euclidean.f90 Sd_frame.f90 Se_status.f90 Sf_def_all_kinds.f90 Sg_sagan_wiggler.f90 Sh_def_kind.f90 Si_def_element.f90 Sk_link_list.f90 Sl_family.f90 Sm_tracking.f90 Sma0_beam_beam_ptc.f90 Sma_multiparticle.f90 Sn_mad_like.f90 So_fitting.f90 Sp_keywords.f90 Sq_orbit_ptc.f90)
+file(GLOB fsrcfiles *.f90 *.F90)
 
-# Note, this only appends -O0 to the compile flags, which includes -O4
-# since gcc only takes last optimization into account, this works for gcc.
-# It will not be a general solution I believe
-set_source_files_properties(matchlib2.f90 PROPERTIES COMPILE_FLAGS "-O0") 
+
+if(MADX_STATIC)
+ set(BLA_STATIC TRUE)
+endif()
+find_package(LAPACK) # (lapack requires blas...)
+if(LAPACK_FOUND)
+    file(GLOB to_remove matchlib.f90 matchlib2.f90)
+    list(REMOVE_ITEM fsrcfiles ${to_remove})
+else()
+    # Note, this only APPENDS -O0 to the compile flags, same as Makefile currently does.
+    set_source_files_properties(matchlib2.f90 PROPERTIES COMPILE_FLAGS "-O0") 
+endif()
 
 # add source files according to NTPSA option...
 if (MADX_NTPSA )
