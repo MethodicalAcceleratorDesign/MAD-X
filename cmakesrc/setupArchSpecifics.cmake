93d7b10eb876c07a2d051563b2a1456dcb2dca66
Yngve Inntjore Levinsen <yngve.levinsen@gmail.com>
2011-07-18 15:44:26 +0000

    r2781: [cmake] - policy settings must be set in top level file.. - cleaned up indendation in arch specifics setup

--- cmakesrc/setupArchSpecifics.cmake
+++ cmakesrc/setupArchSpecifics.cmake
@@ -7,54 +7,54 @@ endif(APPLE)
 
 if ( MADX_FORCE_32 OR ${CMAKE_SIZEOF_VOID_P} EQUAL 4 )
 
-  message("32 bit build" ) 
-  
-  set (CPACK_DEBIAN_PACKAGE_ARCHITECTURE "i386")
-	
-	if ( MADX_STATIC )
-	 set (CPACK_RPM_PACKAGE_ARCHITECTURE "noarch")
-	 # I think this should be correct but it is not tested yet...
-	 #set (CPACK_DEBIAN_PACKAGE_ARCHITECTURE "all")
-	else ( MADX_STATIC )
-	 if ( NOT MADX_FORCE_32 )
-  	set (CPACK_RPM_PACKAGE_ARCHITECTURE "i686")
-	 else ( NOT MADX_FORCE_32 )
-		 message(WARNING "Don't use CPACK to generate RPM, it will make no sense for these settings.")
-	 endif ( NOT MADX_FORCE_32 )
-	endif ( MADX_STATIC )
-  
-  set (CMAKE_C_FLAGS "${CMAKE_C_FLAGS} -m32")
-  set (CMAKE_CXX_FLAGS "${CMAKE_CXX_FLAGS} -m32")
-  if (CMAKE_Fortran_COMPILER MATCHES "lf95")
-    set (CMAKE_Fortran_FLAGS "${CMAKE_Fortran_FLAGS} -Wa,--32 --chk aesux ")
-  elseif (CMAKE_Fortran_COMPILER MATCHES "nagfor")
-    set (CMAKE_Fortran_FLAGS "${CMAKE_Fortran_FLAGS}  -Wc,-m32 -Wl,-m32 -abi=32")
-  elseif (CMAKE_Fortran_COMPILER MATCHES "ifort")
-    set (CMAKE_Fortran_FLAGS "${CMAKE_Fortran_FLAGS} -fp-model precise -m32")
-  elseif (CMAKE_Fortran_COMPILER MATCHES "gfortran")
-    set (CMAKE_Fortran_FLAGS "${CMAKE_Fortran_FLAGS} -m32")
-  endif (CMAKE_Fortran_COMPILER MATCHES "lf95")
-  set (CMAKE_C_FLAGS "${CMAKE_C_FLAGS} -m32 ")
-  
-  if (CMAKE_SYSTEM_NAME STREQUAL "Linux" AND MADX_STATIC)
-    LINK_DIRECTORIES(${CMAKE_SOURCE_DIR}/lib)
-  endif (CMAKE_SYSTEM_NAME STREQUAL "Linux" AND MADX_STATIC)
+    message(STATUS "32 bit build" ) 
+    
+    set (CPACK_DEBIAN_PACKAGE_ARCHITECTURE "i386")
+            
+    if ( MADX_STATIC )
+        set (CPACK_RPM_PACKAGE_ARCHITECTURE "noarch")
+        # I think this should be correct but it is not tested yet...
+        #set (CPACK_DEBIAN_PACKAGE_ARCHITECTURE "all")
+    else ()
+        if ( NOT MADX_FORCE_32 )
+        set (CPACK_RPM_PACKAGE_ARCHITECTURE "i686")
+        else ()
+            message(WARNING "Don't use CPACK to generate RPM, it will make no sense for these settings.")
+        endif ()
+    endif ()
+    
+    set (CMAKE_C_FLAGS "${CMAKE_C_FLAGS} -m32")
+    set (CMAKE_CXX_FLAGS "${CMAKE_CXX_FLAGS} -m32")
+
+    if (CMAKE_Fortran_COMPILER MATCHES "lf95")
+        set (CMAKE_Fortran_FLAGS "${CMAKE_Fortran_FLAGS} -Wa,--32 --chk aesux ")
+    elseif (CMAKE_Fortran_COMPILER MATCHES "nagfor")
+        set (CMAKE_Fortran_FLAGS "${CMAKE_Fortran_FLAGS}  -Wc,-m32 -Wl,-m32 -abi=32")
+    elseif (CMAKE_Fortran_COMPILER MATCHES "ifort")
+        set (CMAKE_Fortran_FLAGS "${CMAKE_Fortran_FLAGS} -fp-model precise -m32")
+    elseif (CMAKE_Fortran_COMPILER MATCHES "gfortran")
+        set (CMAKE_Fortran_FLAGS "${CMAKE_Fortran_FLAGS} -m32")
+    endif ()
+    
+    if (CMAKE_SYSTEM_NAME STREQUAL "Linux" AND MADX_STATIC)
+        LINK_DIRECTORIES(${CMAKE_SOURCE_DIR}/lib)
+    endif ()
   
 elseif (${CMAKE_SIZEOF_VOID_P} EQUAL 8)
 
-  message("64 bit build")
-  
-	 set (CPACK_DEBIAN_PACKAGE_ARCHITECTURE "amd64")
-   set (CPACK_RPM_PACKAGE_ARCHITECTURE "x86_64")
-   
-  if (CMAKE_Fortran_COMPILER MATCHES "lf95")
-    set (CMAKE_Fortran_FLAGS "${CMAKE_Fortran_FLAGS} --chk aefs")
-  endif (CMAKE_Fortran_COMPILER MATCHES "lf95")
-  
-  if (CMAKE_SYSTEM_NAME STREQUAL "Linux" AND MADX_STATIC)
-	   LINK_DIRECTORIES(${CMAKE_SOURCE_DIR}/lib64)
-  endif (CMAKE_SYSTEM_NAME STREQUAL "Linux" AND MADX_STATIC)
- endif ( MADX_FORCE_32   OR ${CMAKE_SIZEOF_VOID_P} EQUAL 4 )
+    message(STATUS "64 bit build")
+    
+    set (CPACK_DEBIAN_PACKAGE_ARCHITECTURE "amd64")
+    set (CPACK_RPM_PACKAGE_ARCHITECTURE "x86_64")
+    
+    if (CMAKE_Fortran_COMPILER MATCHES "lf95")
+        set (CMAKE_Fortran_FLAGS "${CMAKE_Fortran_FLAGS} --chk aefs")
+    endif ()
+    
+    if (CMAKE_SYSTEM_NAME STREQUAL "Linux" AND MADX_STATIC)
+            LINK_DIRECTORIES(${CMAKE_SOURCE_DIR}/lib64)
+    endif ()
+endif ()
 
 
 # OSX specifics:
--- cmakesrc/setupVersion.cmake
+++ cmakesrc/setupVersion.cmake
@@ -1,7 +1,3 @@
-if(COMMAND cmake_policy)
-  cmake_policy(set CMP0003 NEW)
-endif(COMMAND cmake_policy)
-
 
 # project version
 set( PROJECT_MAJOR_VERSION 5 )
@@ -11,7 +7,7 @@ set( PROJECT_PATCH_LEVEL 07 )
 
 
 if ( NOT PROJECT_PATCH_LEVEL EQUAL 00 )
- message( "Building a development version" )
- set (BINARY_POSTFIX "_dev")
- set (PKG_POSTFIX "-dev")
+    message(STATUS "Building a development version" )
+    set (BINARY_POSTFIX "_dev")
+    set (PKG_POSTFIX "-dev")
 endif ( NOT PROJECT_PATCH_LEVEL EQUAL 00 )
