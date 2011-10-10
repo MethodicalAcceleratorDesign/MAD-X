39651936e81eace449035df608821a355f6de571
Yngve Inntjore Levinsen <yngve.levinsen@gmail.com>
2011-10-10 06:49:35 +0000

    r2812: removed configure warning on some systems

--- cmakesrc/setupWrappers.cmake
+++ cmakesrc/setupWrappers.cmake
@@ -8,8 +8,11 @@
 #execute python wrapper scripts (you need to be dependent on one of the output files or else this command will never be ran):
 # Unsure about dependencies.. Might be an overkill this one.
 find_package(PythonInterp REQUIRED)
-if(${PYTHON_VERSION_MAJOR} EQUAL 3)
-    set(PYSCRPT_END "_py3")
+
+if(PYTHON_VERSION_MAJOR) # for older cmake this variable is not defined..
+    if(${PYTHON_VERSION_MAJOR} EQUAL 3)
+        set(PYSCRPT_END "_py3")
+    endif()
 endif()
 ADD_CUSTOM_COMMAND(
   OUTPUT c_wrappers.c c_wrappers.h c_prototypes.h c_wrappers_prototypes.h
