2f347bb3b8d6bf5b7eb6d6e51cf260ce930d36b9
Laurent Deniau <Laurent.Deniau@cern.ch>
2011-11-08 15:49:00 +0000

    r2861: lahey support

--- mad_main.c
+++ mad_main.c
@@ -13,12 +13,31 @@ char**  mad_argv;
 // readonly global information about program's stack
 void*   mad_stck_base;
 
+#ifdef _GFORTRAN
+void _gfortran_set_args (int argc, char *argv[]);
+#endif
+
+#ifdef _LF95
+// Lahey f95 compiler specific
+int
+MAIN__()
+{
+  int a;
+  mad_stck_base = &a;
+  mad_argc = 0;
+  mad_argv = 0;
+#else
 int
 main(int argc, char *argv[])
 {
   mad_stck_base = &argc;
   mad_argc = argc;
   mad_argv = argv;
+#endif
+
+#ifdef _GFORTRAN
+  _gfortran_set_args(argc, argv);
+#endif
 
   madx_start();
   madx_input(CALL_LEVEL_ZERO);
