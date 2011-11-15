980bd9b59cd04adaad2c9ca94b5e848713be9949
Laurent Deniau <Laurent.Deniau@cern.ch>
2011-11-15 10:50:07 +0000

    r2922: file restored for Win32

---
+++ gxx11psc.c
@@ -0,0 +1,42 @@
+#include <stdio.h>
+#include <time.h>
+
+#ifdef _WRAP_FORTRAN_CALLS
+#include "fortran_wrappers.h"
+#endif
+#ifdef _WRAP_C_CALLS
+#include "c_wrappers.h"
+#endif
+
+#define cbyt     cbyt_
+#define mydtime  mydtime_
+void cbyt(int* source, int* s_pos, int* target, int* t_pos, int* n)
+/* inserts n_bit byte from source at position s_pos in target at t_pos.
+   Attention: least significant bit is #1, positions are those of the least
+   significant bit in byte */
+{
+  int mask = 1;
+  int so = *source, tg = *target;
+  so >>= (*s_pos - 1);
+  mask <<= *n;
+  mask -= 1;
+  so &= mask;
+  mask <<= (*t_pos - 1);  so <<= (*t_pos - 1);
+  tg &= ~mask; tg |= so;
+  *target = tg;
+}
+
+void mydtime(int* year, int* month, int* day, int* hour, int* minute,
+             int* sec)
+{
+  time_t _time;
+  struct tm* tm;
+  time(&_time); /* initialize timing */
+  tm = localtime(&_time); /* split system time */
+  *year = tm->tm_year%100;
+  *month = tm->tm_mon+1;
+  *day = tm->tm_mday;
+  *hour = tm->tm_hour;
+  *minute = tm->tm_min;
+  *sec = tm->tm_sec;
+}
