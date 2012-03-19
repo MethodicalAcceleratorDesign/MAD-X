edd82091aa10ab1a449b7f2f269a4c9926a3450e
Laurent Deniau <Laurent.Deniau@cern.ch>
2012-03-19 14:58:01 +0000

    r3217: PTC correction (logical)

--- src/Sp_keywords.f90
+++ src/Sp_keywords.f90
@@ -1981,7 +1981,7 @@ do i=1,ring%n
   call el_el0(f%mag,my_true,mf)
   call fib_fib0(f,my_true,mf)
   CALL MC_MC0(f%MAG%P,my_true,mf)
-  CALL print_ElementLIST(f%mag,MY_TRUE,mf)
+  CALL print_ElementLIST(f%mag,.true.,mf)
  f=>f%next    
 enddo
 
@@ -2000,7 +2000,7 @@ do i=1,ring%n
   call el_el0(f%mag,my_false,mf)
   call fib_fib0(f,my_false,mf)
   CALL MC_MC0(f%MAG%P,my_false,mf) 
-  CALL print_ElementLIST(f%mag,my_false,mf)
+  CALL print_ElementLIST(f%mag,.false.,mf)
 
  
  
@@ -2194,7 +2194,7 @@ end subroutine el_el0
     implicit none
     type(element), pointer :: el
     integer mf,i
-    LOGICAL dir
+    LOGICAL(KIND=4) dir
     character*255 line
 
 
