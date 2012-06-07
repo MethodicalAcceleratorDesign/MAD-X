2bafa6c4ea5f7a24f3dbf919923b0de6d80a3814
Laurent Deniau <Laurent.Deniau@cern.ch>
2012-06-07 15:24:01 +0000

    r3292: C99 problem on Windows

--- src/mad_port.h
+++ src/mad_port.h
@@ -2,16 +2,22 @@
 #define MAD_PORT_H
 
 #ifndef _WIN32
+// problem with C99 compliance on Windows
 #include <stdint.h>
 #endif
 
-#ifdef _ICC
+#ifdef _WIN32
+// problem with C99 compliance on Windows
+#if !__STDC__ || __STDC_VERSION__ < 199901L
+extern double rint(double);
+#endif
+#endif
+
 // problem with non-standard Intel names in math.h
+#ifdef _ICC
 #define compound(a,b) compound_intel(a,b)
 #include <math.h>
 #undef  compound
-#else
-#include <math.h>
 #endif
 
-#endif
+#endif // MAD_PORT_H
