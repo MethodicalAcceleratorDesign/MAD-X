0b4694c86d552f0dcf526650f759409a19c30c3c
Yngve Inntjore Levinsen <yngve.levinsen@gmail.com>
2012-06-12 14:43:58 +0000

    r3332: moved all cmake related files from tests directory

--- cmake/numdifftests.cmake
+++ cmake/numdifftests.cmake
@@ -1,9 +1,10 @@
 get_target_property(binaryname madxbin LOCATION)
 get_target_property(ndiffbin numdiff LOCATION)
 
-set(BASESCRIPT ${CMAKE_SOURCE_DIR}/cmake/ctestbase.cmake)
 execute_process(COMMAND ${CMAKE_COMMAND} -E create_symlink 
-   ${CMAKE_SOURCE_DIR}/examples ${CMAKE_CURRENT_BINARY_DIR}/examples)
+   ${CMAKE_SOURCE_DIR}/examples ${CMAKE_BINARY_DIR}/examples)
+
+set(BASESCRIPT ${CMAKE_SOURCE_DIR}/cmake/ctestbase.cmake)
 
 macro(numdiff_test testname islong)
    if(${islong})
@@ -12,13 +13,22 @@ macro(numdiff_test testname islong)
       set(_testname ${testname})
    endif()
    execute_process(COMMAND ${CMAKE_COMMAND} -E copy_directory 
-      ${CMAKE_CURRENT_SOURCE_DIR}
-      ${CMAKE_CURRENT_BINARY_DIR})
+      ${CMAKE_CURRENT_SOURCE_DIR}/tests/${testname}
+      ${CMAKE_CURRENT_BINARY_DIR}/tests/${testname})
    add_test(${_testname}
        ${CMAKE_COMMAND}
        -DTEST_PROG=${binaryname}
-       -DSOURCEDIR=${CMAKE_CURRENT_BINARY_DIR}/${testname}
+       -DSOURCEDIR=${CMAKE_CURRENT_BINARY_DIR}/tests/${testname}
        -DTEST_NAME=${testname}
        -DNUMDIFF=${ndiffbin}
        -P ${BASESCRIPT})
 endmacro()
+
+# First parameter is test name,
+# second is a bool saying if it is 
+# a long test (test-user)
+
+numdiff_test(test-jacobian False)
+numdiff_test(test-jacobian-2 False)
+numdiff_test(test-jacobian-knobs False)
+numdiff_test(test-ptc-twiss False)
