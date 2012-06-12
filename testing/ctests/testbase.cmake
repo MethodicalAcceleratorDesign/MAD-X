099b0f791d88169d9fb9c2a24e8055b675c0a25d
Yngve Inntjore Levinsen <yngve.levinsen@gmail.com>
2012-06-12 08:04:09 +0000

    r3318: cleaned up ctests, now getting files directly from examples directory..

--- testing/ctests/testbase.cmake
+++ testing/ctests/testbase.cmake
@@ -1,10 +1,10 @@
 execute_process(COMMAND cat ${SOURCEDIR}/${TEST_SCRIPT} COMMAND ${TEST_PROG}
                 RESULT_VARIABLE HAD_ERROR)
+
 if(HAD_ERROR)
     message(FATAL_ERROR "Test failed with error ${HAD_ERROR}")
 endif()
 
-# This will always fail because date/version is included in output...
 if(TEST_OUTPUT)
     find_package(PythonInterp)
     if(PYTHONINTERP_FOUND)
