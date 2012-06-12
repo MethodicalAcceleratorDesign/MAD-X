e3812bc879c91785cc8bd18b6a3aa0c330359d14
Yngve Inntjore Levinsen <yngve.levinsen@gmail.com>
2012-06-12 13:39:06 +0000

    r3328: added new cmake build DebugCoverage..

--- cmake/madxInit.cmake
+++ cmake/madxInit.cmake
@@ -37,3 +37,9 @@ if(NOT MADX_PATCH_LEVEL EQUAL 00)
     set (BINARY_POSTFIX "_dev")
     set (PKG_POSTFIX "-dev")
 endif()
+
+# Build type for Coverage checks:
+set(CMAKE_CXX_FLAGS_DEBUGCOVERAGE "${CMAKE_CXX_FLAGS_DEBUG} -Wall -fprofile-arcs -ftest-coverage")
+set(CMAKE_C_FLAGS_DEBUGCOVERAGE "${CMAKE_C_FLAGS_DEBUG} -Wall -fprofile-arcs -ftest-coverage")
+set(CMAKE_Fortran_FLAGS_DEBUGCOVERAGE "${CMAKE_Fortran_FLAGS_DEBUG} -Wall -fprofile-arcs -ftest-coverage")
+set(LINK_FLAGS_DEBUGCOVERAGE "${LINK_FLAGS_DEBUG} -Wall -fprofile-arcs -ftest-coverage")
