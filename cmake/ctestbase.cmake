d5612c6fb158e715eebc09affe5366ad3b0a7427
Yngve Inntjore Levinsen <yngve.levinsen@gmail.com>
2012-06-20 14:48:59 +0000

    r3366: fixed ctest to accomodate new tests

--- cmake/ctestbase.cmake
+++ cmake/ctestbase.cmake
@@ -1,10 +1,14 @@
 execute_process(COMMAND cat ${SOURCEDIR}/${TEST_NAME}.madx COMMAND ${TEST_PROG}
-   OUTPUT_FILE "out.txt" WORKING_DIRECTORY ${SOURCEDIR} RESULT_VARIABLE HAD_ERROR)
+   OUTPUT_FILE "${TEST_NAME}.out" WORKING_DIRECTORY ${SOURCEDIR} RESULT_VARIABLE HAD_ERROR)
+
+# We must make a list so it is understood as multiple
+# input arguments..
+string(REGEX REPLACE " " ";" _TEST_OUTPUT ${TEST_OUTPUT})
 
 if(HAD_ERROR)
     message(FATAL_ERROR "Test failed with error ${HAD_ERROR}")
- else()
-    execute_process(COMMAND ${NUMDIFF} -q -b -c -n -t ${TEST_NAME} out.txt ref.txt cfg.txt  WORKING_DIRECTORY ${SOURCEDIR} RESULT_VARIABLE NUMDIFF_ERROR)
+    else()
+    execute_process(COMMAND ${NUMDIFF} -q -b -c -l -n -t ${TEST_NAME} ${_TEST_OUTPUT} WORKING_DIRECTORY ${SOURCEDIR} RESULT_VARIABLE NUMDIFF_ERROR)
     if(NUMDIFF_ERROR)
        message(FATAL_ERROR "Test failed with numdiff error ${NUMDIFF_ERROR}")
     endif()
--- cmake/numdifftests.cmake
+++ cmake/numdifftests.cmake
@@ -6,39 +6,42 @@ execute_process(COMMAND ${CMAKE_COMMAND} -E create_symlink
 
 set(BASESCRIPT ${CMAKE_SOURCE_DIR}/cmake/ctestbase.cmake)
 
-macro(numdiff_test testname islong)
+macro(numdiff_test testname testout islong)
    if(${islong})
       set(_testname ${testname}_LONG)
    else()
       set(_testname ${testname})
    endif()
+   set(_testout "${testname} ${testout}")
    execute_process(COMMAND ${CMAKE_COMMAND} -E copy_directory 
       ${CMAKE_CURRENT_SOURCE_DIR}/tests/${testname}
       ${CMAKE_CURRENT_BINARY_DIR}/tests/${testname})
    add_test(${_testname}
        ${CMAKE_COMMAND}
        -DTEST_PROG=${binaryname}
+       -DTEST_OUTPUT=${_testout}
        -DSOURCEDIR=${CMAKE_CURRENT_BINARY_DIR}/tests/${testname}
        -DTEST_NAME=${testname}
        -DNUMDIFF=${ndiffbin}
        -P ${BASESCRIPT})
 endmacro()
 
-# First parameter is test name,
-# second is a bool saying if it is 
-# a long test (test-user)
+# First parameter is test name.
+# Second is optionally additional output file names.
+# Third is a bool saying if it is 
+# a long test (test-user).
 
-numdiff_test(test-ibs False)
-numdiff_test(test-jacobian False)
-numdiff_test(test-jacobian-2 False)
-numdiff_test(test-jacobian-knobs False)
-numdiff_test(test-ptc-twiss False)
-numdiff_test(test-ptc-normal False)
+numdiff_test(test-ibs "ibs_output.tfs" False)
+numdiff_test(test-jacobian "" False)
+numdiff_test(test-jacobian-2 "" False)
+numdiff_test(test-jacobian-knobs "knobfile" False)
+numdiff_test(test-ptc-twiss "Maxwellian_bend_for_ptc.txt fort.18 ptc-twiss-table" False)
+numdiff_test(test-ptc-normal "distort_1_f_end distort_1_h_end fc.3 fort.18 ptc_map_table.tfs ptc_normal_results.tfs" False)
 
 # Tests that require afs:
 if(EXISTS "/afs/cern.ch/")
-   numdiff_test(test-twiss False)
-   numdiff_test(test-match True)
+   numdiff_test(test-twiss "sample_optics.tfs" False)
+   numdiff_test(test-match "str.ip8.b1.dat test-match twiss.ir8.b1.data" True)
 else()
    message(STATUS "afs is not available, some tests will be missing")
 endif()
