1540aa54733767fcf16fc4c547eb6a5c3484586a
Yngve Inntjore Levinsen <yngve.levinsen@gmail.com>
2012-06-21 08:14:57 +0000

    r3375: removed cmake policy not available on older versions

--- cmake/numdifftests.cmake
+++ cmake/numdifftests.cmake
@@ -33,17 +33,17 @@ endmacro()
 # Third is a bool saying if it is 
 # a long test (test-user).
 
-numdiff_test(test-ibs "ibs_output.tfs" False)
-numdiff_test(test-jacobian "" False)
-numdiff_test(test-jacobian-2 "" False)
-numdiff_test(test-jacobian-knobs "knobfile" False)
-numdiff_test(test-ptc-twiss "Maxwellian_bend_for_ptc.txt fort.18 ptc-twiss-table" False)
-numdiff_test(test-ptc-normal "distort_1_f_end distort_1_h_end fc.3 fort.18 ptc_map_table.tfs ptc_normal_results.tfs" False)
+numdiff_test(test-ibs "ibs_output.tfs" 0)
+numdiff_test(test-jacobian "" 0)
+numdiff_test(test-jacobian-2 "" 0)
+numdiff_test(test-jacobian-knobs "knobfile" 0)
+numdiff_test(test-ptc-twiss "Maxwellian_bend_for_ptc.txt fort.18 ptc-twiss-table" 0)
+numdiff_test(test-ptc-normal "distort_1_f_end distort_1_h_end fc.3 fort.18 ptc_map_table.tfs ptc_normal_results.tfs" 0)
 
 # Tests that require afs:
 if(EXISTS "/afs/cern.ch/")
-   numdiff_test(test-twiss "sample_optics.tfs" False)
-   numdiff_test(test-match "str.ip8.b1.dat test-match twiss.ir8.b1.data" True)
+   numdiff_test(test-twiss "sample_optics.tfs" 0)
+   numdiff_test(test-match "str.ip8.b1.dat test-match twiss.ir8.b1.data" 1)
 else()
    message(STATUS "afs is not available, some tests will be missing")
 endif()
