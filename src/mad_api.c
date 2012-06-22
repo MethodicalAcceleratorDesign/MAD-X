2f66256a89a4c36f1cabdb8e11ac6d568415f49b
Yngve Inntjore Levinsen <yngve.levinsen@gmail.com>
2012-06-22 17:05:53 +0000

    r3398: removed unneeded functions from mad_api.c/h updated documentation in mad_api.h

--- src/mad_api.c
+++ src/mad_api.c
@@ -1,28 +1,8 @@
 #include "madx.h"
 #include "mad_api.h"
 
-void
-madextern_start(void)
-{
-    madx_start();
-}
-
 struct sequence_list*
 madextern_get_sequence_list(void)
 {
     return sequences;
 }
-
-void
-madextern_end(void)
-{
-    madx_finish();
-}
-
-void
-madextern_input(char* ch) 
-{
-    stolower_nq(ch);
-    pro_input(ch);
-}
-
--- src/mad_api.h
+++ src/mad_api.h
@@ -1,70 +1,31 @@
 #ifndef MAD_API_H
 #define MAD_API_H
 
+/**
+ * @file mad_api.h Calls to use for external libraries
+ *
+ * This is a header that can be included in external libraries that wants to 
+ * link against the mad-x library. It was mostly needed because
+ * we did not have clean headers before. 
+ * 
+ * Since that has now changed, have a look instead at the functions 
+ * madx_start() and madx_finish() in mad_core.h, 
+ * and pro_input() in mad_eval.h. Those provide everything
+ * you need for a basic interface.
+ * 
+ * @author Yngve Inntjore Levinsen
+ */
+
 // types
 
 struct sequence_list;
 
 // variables
 
-/**
- * @file madextern.h Calls to use for external libraries
- *
- * This is a header that can be included in external libraries that wants to 
- * link against the mad-x library. When you want to send commands to madX you must
- * first start with the command madextern_start(), before you can parse
- * character pointers to the command madextern_input(char*).
- * You must free memory before exiting with the function madextern_end(),
- * otherwise you will get a segmentation fault.
- *
- * This library can be extended with commands like e.g. madextern_twiss().
- * Add declaration in this file (and preferably documentation), and then implement
- * it in madxp.c or another preferred source file. Always use naming convention
- * madxp_nameOfFunc(), and always declare the function extern.
- */
 extern struct sequence_list* sequences;
 
-// interface
-
 /**
- * @brief Initialize the mad-x library.
- * 
- * This function is part of the madextern external functions.
- * These are not supposed to be used by any internal mad-x code.
- *
- * This function must be called before you can call other
- * functions in the mad-x library.
- */
-void madextern_start(void);
-
-/**
- * @brief Close mad-x library
- *
- * This function is part of the madextern external functions.
- * These are not supposed to be used by any internal mad-x code.
- *
- * Use this function as the last command when you want to free memory used by objects
- * in mad-x.
- */
-void madextern_end(void);
-
-/**
- * @brief Send command to the mad-x library.
- *
- * This function is part of the madextern external functions.
- * These are not supposed to be used by any internal mad-x code.
- *
- * This function works in the same way as the mad-x interpreter,
- * ie you send a command like e.g. 'call,file="filename.madx";'
- * Note that commands should have a ';' at the end, and that several
- * commands can be sent at once separated by ';'
- *
- * @param ch The command to execute
- */
-void madextern_input(char*);
-
-/**
- * @brief Send command to the mad-x library.
+ * @brief Get list of available sequences
  *
  * This function is part of the madextern external functions.
  * These are not supposed to be used by any internal mad-x code.
