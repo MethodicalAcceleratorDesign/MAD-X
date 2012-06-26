cf6eb2df6ab26db57444bb2b181fbf17d993d255
Yngve Inntjore Levinsen <yngve.levinsen@gmail.com>
2012-06-26 15:51:54 +0000

    r3407: externalproject pymad installing as part of the "make install" process, instead of immediately

--- cmake/ExternalProject-pymad.cmake
+++ cmake/ExternalProject-pymad.cmake
@@ -4,9 +4,6 @@ find_package(PythonInterp REQUIRED)
 find_package(CYTHON REQUIRED)
 
 set(PYMAD_BUILD_COMMAND ${PYTHON_EXECUTABLE} setup.py build)
-set(PYMAD_INSTALL_COMMAND ${PYTHON_EXECUTABLE} setup.py install)
-
-get_target_property(LIBLOCATION madx LOCATION)
 
 ExternalProject_Add(
    pymad
@@ -17,5 +14,11 @@ ExternalProject_Add(
    BINARY_DIR pymad/src/
    SOURCE_DIR pymad
    BUILD_COMMAND ${PYMAD_BUILD_COMMAND}
-   INSTALL_COMMAND ${PYMAD_INSTALL_COMMAND}
+   INSTALL_COMMAND ""
 )
+
+ExternalProject_Get_Property(pymad binary_dir)
+
+set(PYMAD_INSTALL_COMMAND ${PYTHON_EXECUTABLE} setup.py install --prefix=${CMAKE_INSTALL_PREFIX})
+
+install(CODE "execute_process(COMMAND ${PYMAD_INSTALL_COMMAND} WORKING_DIRECTORY ${binary_dir})" COMPONENT Runtime)
