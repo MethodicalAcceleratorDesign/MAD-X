b9b5d2bfc7638d3eae07dc208cb7332dd766b0d8
Yngve Inntjore Levinsen <yngve.levinsen@gmail.com>
2012-07-03 12:52:07 +0000

    r3447: fixed ctestbase for windows

--- cmake/ctestbase.cmake
+++ cmake/ctestbase.cmake
@@ -1,14 +1,19 @@
-execute_process(COMMAND cat ${SOURCEDIR}/${TEST_NAME}.madx COMMAND ${TEST_PROG}
-   OUTPUT_FILE "${TEST_NAME}.out" WORKING_DIRECTORY ${SOURCEDIR} RESULT_VARIABLE HAD_ERROR)
+if(WIN32)
+	set(_CAT cmd.exe /c type)
+else()
+	set(_CAT cat)
+endif()
+execute_process(COMMAND ${_CAT} ${SOURCEDIR}/${TEST_NAME}.madx COMMAND ${TEST_PROG}
+   OUTPUT_FILE ${TEST_NAME}.out WORKING_DIRECTORY ${SOURCEDIR} RESULT_VARIABLE HAD_ERROR)
 
 # We must make a list so it is understood as multiple
 # input arguments..
 string(REGEX REPLACE " " ";" _TEST_OUTPUT ${TEST_OUTPUT})
-
 if(HAD_ERROR)
     message(FATAL_ERROR "Test failed with error ${HAD_ERROR}")
-    else()
-    execute_process(COMMAND ${NUMDIFF} -q -b -c -l -n -t ${TEST_NAME} ${_TEST_OUTPUT} WORKING_DIRECTORY ${SOURCEDIR} RESULT_VARIABLE NUMDIFF_ERROR)
+else()
+	execute_process(COMMAND ${NUMDIFF} -q -b -c -l -n -t ${TEST_NAME} ${_TEST_OUTPUT}
+	    WORKING_DIRECTORY ${SOURCEDIR} RESULT_VARIABLE NUMDIFF_ERROR)
     if(NUMDIFF_ERROR)
        message(FATAL_ERROR "Test failed with numdiff error ${NUMDIFF_ERROR}")
     endif()
