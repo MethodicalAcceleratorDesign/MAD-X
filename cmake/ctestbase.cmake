b6fb6b3394dc25b9f1e88641574637341295ba04
Yngve Inntjore Levinsen <yngve.levinsen@gmail.com>
2012-07-03 13:00:08 +0000

    r3451: numdiff test in ctest prints the commands before they are ran

--- cmake/ctestbase.cmake
+++ cmake/ctestbase.cmake
@@ -3,6 +3,7 @@ if(WIN32)
 else()
 	set(_CAT cat)
 endif()
+message("COMMAND ${TEST_PROG} < ${SOURCEDIR}/${TEST_NAME}.madx")
 execute_process(COMMAND ${_CAT} ${SOURCEDIR}/${TEST_NAME}.madx COMMAND ${TEST_PROG}
    OUTPUT_FILE ${TEST_NAME}.out WORKING_DIRECTORY ${SOURCEDIR} RESULT_VARIABLE HAD_ERROR)
 
@@ -12,8 +13,9 @@ string(REGEX REPLACE " " ";" _TEST_OUTPUT ${TEST_OUTPUT})
 if(HAD_ERROR)
     message(FATAL_ERROR "Test failed with error ${HAD_ERROR}")
 else()
-	execute_process(COMMAND ${NUMDIFF} -q -b -c -l -n -t ${TEST_NAME} ${_TEST_OUTPUT}
-	    WORKING_DIRECTORY ${SOURCEDIR} RESULT_VARIABLE NUMDIFF_ERROR)
+    message("COMMAND ${NUMDIFF} -q -b -c -l -n -t ${TEST_NAME} ${_TEST_OUTPUT}")
+    execute_process(COMMAND ${NUMDIFF} -q -b -c -l -n -t ${TEST_NAME} ${_TEST_OUTPUT}
+        WORKING_DIRECTORY ${SOURCEDIR} RESULT_VARIABLE NUMDIFF_ERROR)
     if(NUMDIFF_ERROR)
        message(FATAL_ERROR "Test failed with numdiff error ${NUMDIFF_ERROR}")
     endif()
