ec8d19116fe9d3634ca22db386f421c61896dde1
Yngve Inntjore Levinsen <yngve.levinsen@gmail.com>
2012-12-12 09:14:54 +0000

    r3757: ctest remove previous output files if existing..

--- cmake/ctestbase.cmake
+++ cmake/ctestbase.cmake
@@ -1,13 +1,22 @@
-message("COMMAND ${TEST_PROG} < ${SOURCEDIR}/${TEST_NAME}.madx")
-execute_process(COMMAND ${TEST_PROG} INPUT_FILE ${SOURCEDIR}/${TEST_NAME}.madx
-   OUTPUT_FILE ${TEST_NAME}.out WORKING_DIRECTORY ${SOURCEDIR} RESULT_VARIABLE HAD_ERROR)
 
 # We must make a list so it is understood as multiple
 # input arguments..
 string(REGEX REPLACE " " ";" _TEST_OUTPUT ${TEST_OUTPUT})
+
+# Remove files in case this test was ran before..
+foreach(_f ${_TEST_OUTPUT} ${TEST_NAME}.out)
+   file(REMOVE ${SOURCEDIR}/${_f})
+endforeach()
+
+# Run simulation..
+message("COMMAND ${TEST_PROG} < ${SOURCEDIR}/${TEST_NAME}.madx")
+execute_process(COMMAND ${TEST_PROG} INPUT_FILE ${SOURCEDIR}/${TEST_NAME}.madx
+   OUTPUT_FILE ${TEST_NAME}.out WORKING_DIRECTORY ${SOURCEDIR} RESULT_VARIABLE HAD_ERROR)
+
 if(HAD_ERROR)
     message(FATAL_ERROR "Test failed with error ${HAD_ERROR}")
 else()
+    # Run numdiff if all went well..
     message("COMMAND ${NUMDIFF} -q -b -c -l -n -t ${TEST_NAME} ${TEST_OUTPUT}")
     execute_process(COMMAND ${NUMDIFF} -q -b -c -l -n -t ${TEST_NAME} ${_TEST_OUTPUT}
         WORKING_DIRECTORY ${SOURCEDIR} RESULT_VARIABLE NUMDIFF_ERROR)
