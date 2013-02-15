2bfd1ac6981bcc70bae390e480eed1fb7d4b0c99
Laurent Deniau <Laurent.Deniau@cern.ch>
2013-02-15 17:52:00 +0000

    r3938: more tests added

--- src/mad_main.c
+++ src/mad_main.c
@@ -52,8 +52,7 @@ main(int argc, char *argv[])
   mad_run ();
   mad_fini();
 
-  return EXIT_SUCCESS;
-//  return geterrorflag() ? EXIT_FAILURE : EXIT_SUCCESS;
+  return geterrorflag() ? EXIT_FAILURE : EXIT_SUCCESS;
 }
 
 void
