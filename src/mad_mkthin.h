e0a3087ddfa8a696d17a5eb44b276d56ddb379d4
Laurent Deniau <Laurent.Deniau@cern.ch>
2013-07-04 11:45:50 +0000

    r4205: proper header restored

--- src/mad_mkthin.h
+++ src/mad_mkthin.h
@@ -1,4 +1,12 @@
 #ifndef MAD_MKTHIN_H
 #define MAD_MKTHIN_H
-void makethin(struct in_cmd* cmd); // public interface to slice sequence, called by exec_command from mad_cmd.c,   struct in_cmd is defined in mad_cmdin.h
+
+// types
+
+struct in_cmd;
+
+// interface
+
+void makethin(struct in_cmd* cmd);
+
 #endif // MAD_MKTHIN_H
