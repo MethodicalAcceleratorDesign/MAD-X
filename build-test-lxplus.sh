48c1e2ae971c5bbfa2100c87f74531ee481285c9
Laurent Deniau <Laurent.Deniau@cern.ch>
2013-10-14 13:24:29 +0000

    r4360: script to build and tests distributions

--- build-test-lxplus.sh
+++ build-test-lxplus.sh
@@ -2,17 +2,17 @@
 # sh build-test-lxplus.sh 1>| build-test-lxplus.out 2>&1
 # tail -f build-test-lxplus.out
 
-echo "===== SVN update ====="
+echo -e "\n===== SVN update ====="
 svn update
 cat VERSION
 
-echo "===== Gnu build ====="
+echo -e "\n===== Gnu build ====="
 gcc      --version
 g++      --version
 gfortran --version
 make all-linux-gnu 
 
-echo "===== Intel build ====="
+echo -e "\n===== Intel build ====="
 source /afs/cern.ch/sw/IntelSoftware/linux/all-setup.sh ia32
 icc      --version
 ifort    --version
@@ -20,17 +20,17 @@ make all-linux32-intel
 source /afs/cern.ch/sw/IntelSoftware/linux/all-setup.sh intel64
 make all-linux64-intel
 
-echo "===== Dependencies ====="
+echo -e "\n===== Dependencies ====="
 make infodep
 
-echo "===== Gnu tests (32 bit) ====="
+echo -e "\n===== Gnu tests (32 bit) ====="
 make madx-linux32-gnu ; make tests-all ARCH=32
 
-echo "===== Gnu tests (64 bit) ====="
+echo -e "\n===== Gnu tests (64 bit) ====="
 make madx-linux64-gnu ; make tests-all ARCH=64
 
-echo "===== Intel tests (32 bit) ====="
+echo -e "\n===== Intel tests (32 bit) ====="
 make madx-linux32-intel ; make tests-all ARCH=32
 
-echo "===== Intel tests (64 bit) ====="
+echo -e "\n===== Intel tests (64 bit) ====="
 make madx-linux64-intel ; make tests-all ARCH=64
--- build-test-macosx.sh
+++ build-test-macosx.sh
@@ -2,32 +2,32 @@
 # sh build-test-macosx.sh 1>| build-test-macosx.out 2>&1
 # tail -f build-test-macosx.out
 
-echo "===== SVN update ====="
+echo "\n===== SVN update ====="
 svn update
 cat VERSION
 
-echo "===== Gnu build ====="
+echo "\n===== Gnu build ====="
 gcc      --version
 g++      --version
 gfortran --version
 make all-macosx-gnu
 
-echo "===== Intel build ====="
+echo "\n===== Intel build ====="
 icc      --version
 ifort    --version
 make all-macosx-intel
 
-echo "===== Dependencies ====="
+echo "\n===== Dependencies ====="
 make infodep
 
-echo "===== Gnu tests (32 bit) ====="
+echo "\n===== Gnu tests (32 bit) ====="
 make madx-macosx32-gnu ; make tests-all ARCH=32
 
-echo "===== Gnu tests (64 bit) ====="
+echo "\n===== Gnu tests (64 bit) ====="
 make madx-macosx64-gnu ; make tests-all ARCH=64
 
-echo "===== Intel tests (32 bit) ====="
+echo "\n===== Intel tests (32 bit) ====="
 make madx-macosx32-intel ; make tests-all ARCH=32
 
-echo "===== Intel tests (64 bit) ====="
+echo "\n===== Intel tests (64 bit) ====="
 make madx-macosx64-intel ; make tests-all ARCH=64
