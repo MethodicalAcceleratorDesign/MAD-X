8cc6186ffa4c5abd24b3b1c43acd73bcc5dd82bb
Laurent Deniau <Laurent.Deniau@cern.ch>
2013-10-21 16:07:55 +0000

    r4400: improve reports, correct access permission

--- scripts/build-test-report.sh
+++ scripts/build-test-report.sh
@@ -5,17 +5,11 @@
 export LC_CTYPE="C"
 daynum=`date "+%d"`
 
-# cleaning
-rm -f tests-failed.tmp
-
 # look for failed tests on [lxplus | macosx | windows]
 build_test_report ()
 {
 	if [ -s build-test-$1.out ] ; then
-		if [ build-test-$1.out -ot build-test-report.out ] ; then
-			echo "Warning: build-test-$1.out is older than build-test-report.out"
-		fi
-		cp -f build-test-$1.out reports/build-test-$1.out.$daynum
+		cp -f build-test-$1.out tests/reports/build-test-$1.out.$daynum
 		perl -ne '/: FAIL/ && print' build-test-$1.out > $1-failed.tmp
 		if [ -s $1-failed.tmp ] ; then
 			perl -ne '/: FAIL/ && print ; /-> (madx-\S+)/ && print "\n$1:\n"' build-test-$1.out >> tests-failed.tmp
@@ -24,16 +18,24 @@ build_test_report ()
 	fi
 }
 
+# cleaning
+rm -f tests-failed.tmp
+
+# lxplus check
 build_test_report lxplus
 
-scp -B mad@macserv15865.cern.ch:/Users/mad/Projects/madX/build-test-macosx.out .
+# macosx check
+scp -q -B mad@macserv15865.cern.ch:/Users/mad/Projects/madX/build-test-macosx.out .
 build_test_report macosx
 
 # report by email if needed
 if [ -s tests-failed.tmp ] ; then
-	echo "===== Tests Failed =====" >> build-test-report.out
+	echo "===== Tests Failed =====" >  build-test-report.out
 	cat tests-failed.tmp            >> build-test-report.out
-	cat -v build-test-report.out | mail -s "MAD-X builds and tests report" mad-src@cern.ch
+	if [ "$1" != "nomail" ] ; then
+		cat -v build-test-report.out | mail -s "MAD-X builds and tests report" mad-src@cern.ch
+		rm -f build-test-report.out
+	fi
 fi
 
 # cleaning
