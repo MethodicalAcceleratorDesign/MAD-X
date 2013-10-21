931659ff81570f3dfbd2c86845083d0b6ad91b7e
Laurent Deniau <Laurent.Deniau@cern.ch>
2013-10-21 14:55:21 +0000

    r4398: improved report script using function, added report history for one full month

--- scripts/build-test-report.sh
+++ scripts/build-test-report.sh
@@ -1,41 +1,35 @@
 # run:
 # sh scripts/build-test-report.sh
 
+# setup
 export LC_CTYPE="C"
-
-filelist="lxplus-failed.tmp macosx-failed.tmp win-failed.tmp tests-failed.tmp"
+daynum=`date "+%d"`
 
 # cleaning
-rm -f $filelist
+rm -f tests-failed.tmp
 
-# look for failed tests on lxplus
-if [ -s build-test-lxplus.out ] ; then
-	if [ build-test-lxplus.out -ot build-test-report.out ] ; then
-		echo "Warning: build-test-lxplus.out is older than build-test-report.out"
+# look for failed tests on [lxplus | macosx | windows]
+build_test_report ()
+{
+	if [ -s build-test-$1.out ] ; then
+		if [ build-test-$1.out -ot build-test-report.out ] ; then
+			echo "Warning: build-test-$1.out is older than build-test-report.out"
+		fi
+		cp -f build-test-$1.out reports/build-test-$1.out.$daynum
+		perl -ne '/: FAIL/ && print' build-test-$1.out > $1-failed.tmp
+		if [ -s $1-failed.tmp ] ; then
+			perl -ne '/: FAIL/ && print ; /-> (madx-\S+)/ && print "\n$1:\n"' build-test-$1.out >> tests-failed.tmp
+		fi
+		rm -f $1-failed.tmp
 	fi
+}
 
-	perl -ne '/: FAIL/ && print' build-test-lxplus.out > lxplus-failed.tmp
+build_test_report lxplus
 
-	if [ -s lxplus-failed.tmp ] ; then
-		perl -ne '/: FAIL/ && print ; /-> (madx-\S+)/ && print "\n$1:\n"' build-test-lxplus.out >> tests-failed.tmp
-	fi
-fi
-
-# look for failed tests on macosx
 scp -B mad@macserv15865.cern.ch:/Users/mad/Projects/madX/build-test-macosx.out .
+build_test_report macosx
 
-if [ -s build-test-macosx.out ] ; then
-	if [ build-test-macosx.out -ot build-test-report.out ] ; then
-		echo "Warning: build-test-macosx.out is older than build-test-report.out"
-	fi
-
-	perl -ne '/: FAIL/ && print' build-test-macosx.out > macosx-failed.tmp
-
-	if [ -s macosx-failed.tmp ] ; then
-		perl -ne '/: FAIL/ && print ; /-> (madx-\S+)/ && print "\n$1:\n"' build-test-macosx.out >> tests-failed.tmp
-	fi
-fi
-
+# report by email if needed
 if [ -s tests-failed.tmp ] ; then
 	echo "===== Tests Failed =====" >> build-test-report.out
 	cat tests-failed.tmp            >> build-test-report.out
@@ -43,4 +37,4 @@ if [ -s tests-failed.tmp ] ; then
 fi
 
 # cleaning
-rm -f $filelist
+rm -f tests-failed.tmp
