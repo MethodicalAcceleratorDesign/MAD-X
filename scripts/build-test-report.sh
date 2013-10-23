bbd9108206503f4bb3f30ab235789f75a214d621
Laurent Deniau <Laurent.Deniau@cern.ch>
2013-10-23 06:32:05 +0000

    r4417: put explicit links

--- scripts/build-test-report.sh
+++ scripts/build-test-report.sh
@@ -19,9 +19,9 @@ build_test_report ()
 		echo "ERROR: report build-test-$1.out not found (or empty) for platform $1"
 	else
 		cp -f build-test-$1.out tests/reports/${thedate}_build-test-$1.out
-		perl -ne '/: FAIL|ERROR:/ && print' build-test-$1.out > $1-failed.tmp
+		perl -ne '/: FAIL|ERROR: / && print' build-test-$1.out > $1-failed.tmp
 		if [ -s $1-failed.tmp ] ; then
-			perl -ne '/: FAIL/ && print ; /-> (madx-\S+)/ && print "\n$1:\n"' build-test-$1.out >> tests-failed.tmp
+			perl -ne '/: FAIL|ERROR: / && print ; /-> (madx-\S+)/ && print "\n$1:\n"' build-test-$1.out >> tests-failed.tmp
 		fi
 		rm -f $1-failed.tmp
 	fi
@@ -34,17 +34,20 @@ rm -f tests-failed.tmp
 build_test_report lxplus
 
 # macosx check
-scp -q -B mad@macserv15865.cern.ch:Projects/madX/build-test-macosx.out .
+scp -q mad@macserv15865.cern.ch:Projects/madX/build-test-macosx.out .
 build_test_report macosx
 
 # report by email if needed
 if [ ! -s tests-failed.tmp ] ; then
 	echo "ERROR: cannot make build-test-report.out, no input found"
 else
-	echo "===== Tests Failed =====" >  build-test-report.out
-	date                            >> build-test-report.out
-	echo "see files ${thedate}_build-test-*.out at http://cern.ch/madx/madX/tests/reports for details" >> build-test-report.out
-	cat tests-failed.tmp            >> build-test-report.out
+	echo "===== Tests Failed ====="                > build-test-report.out
+	date                                          >> build-test-report.out
+	echo "For details, see report files:"         >> build-test-report.out
+	echo "http://cern.ch/madx/madX/tests/reports/${thedate}_build-test-lxplus.out" >> build-test-report.out
+	echo "http://cern.ch/madx/madX/tests/reports/${thedate}_build-test-macosx.out" >> build-test-report.out
+	echo "http://cern.ch/madx/madX/tests/reports" >> build-test-report.out
+	cat tests-failed.tmp                          >> build-test-report.out
 	if [ "$1" != "nomail" ] ; then
 		cat -v build-test-report.out | mail -s "MAD-X builds and tests report" laurent.deniau@cern.ch # mad-src@cern.ch
 		rm -f build-test-report.out
