826fc6e6d5b6745991141cb0ee5a710aea1feb35
Roy Ghislain <Ghislain.Roy@cern.ch>
2013-12-18 15:30:56 +0000

    r4576: cleanup tests for orphaned and unnecessary files

--- tests/test-thick-quad-3/cmp.sh
+++
@@ -1,8 +0,0 @@
-for PARAM1 in 5 50 500 5000 ; do
-echo "NSLICES = $PARAM1"
-sed "s/PARAM1/$PARAM1/" fivecell.madx > tmp
-time ../../madx < tmp > out_$PARAM1
-done
-
-echo "THICK"
-time ../../madx test-thick-quad-3.madx > out_thick
