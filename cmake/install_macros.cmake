31de7310e262f45e5337fcf9738d0c31956c3dd2
Yngve Inntjore Levinsen <yngve.levinsen@gmail.com>
2014-02-20 09:19:28 +0000

    r4693: cmake: always install headers as well..

--- cmake/install_macros.cmake
+++ cmake/install_macros.cmake
@@ -16,10 +16,8 @@ endmacro()
 
 macro(madx_install_headers)
     # This installs the header files to <prefix>/include/madX
-    # We only want this in the development version...
-    if(NOT ${MADX_PATCH_LEVEL} EQUAL 00)
         install(FILES ${ARGN} 
                 DESTINATION "include/${PROJECT_NAME}"
                 COMPONENT Files)
-    endif()
 endmacro()
+
