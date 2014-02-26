5e85948be6539ec17805662ab649198dca06db03
Laurent Deniau <Laurent.Deniau@cern.ch>
2014-02-26 14:06:55 +0000

    r4700: valgrind script updated to run in copy of tests because tests are too long (few days)

--- scripts/test-valgrind.sh
+++ scripts/test-valgrind.sh
@@ -1,6 +1,7 @@
 # run:
 # sh scripts/test-valgrind.sh [madx]
 
+readonly testdir="tests-valgrind"
 readonly thedate=`date "+%Y-%m-%d"`
 readonly summary="valgrind-summary.txt"
 readonly ext="valgrind"
@@ -12,14 +13,15 @@ madx="$1"
 [ ! -x "$madx" ] && echo "error: invalid program '$madx' for tests with valgrind" && exit 1
 
 # go to tests directory
-cd tests
+cd $testdir
+[ "$?" != "0" ] && echo "error: copy 'tests' tree into '$testdir' before running tests with valgrind" && exit 1
 
 # clean summary, set the date
-echo $thedate > $summary
+echo "tests started at $thedate" > $summary
 
 # run all tests with valgrind
 for i in test-*; do
-  echo "running test $i (produce $i.$ext)"
+  echo "running test $i (produce $i.$ext): `date`"
   cd $i
   valgrind -v --leak-check=full --track-origins=yes ../../$madx $i.madx > $i.$ext 2>&1
   grep -E "$pattern" $i.$ext /dev/null >> ../$summary
