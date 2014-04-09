4868c9255062cb1dbec6be207e22c80bd86abcbb
Roy Ghislain <Ghislain.Roy@cern.ch>
2014-04-09 14:37:30 +0000

    r4745: #259 bugfix for CIRC and FREQ0 get zeroed when updating NPART in BEAM command. test and doc updated.

--- src/mad_beam.c
+++ src/mad_beam.c
@@ -37,24 +37,22 @@ exec_beam(struct in_cmd* cmd, int flag)
   struct name_list* nl = cmd->clone->par_names;
   int pos = name_list_pos("sequence", nl);
   int bpos = name_list_pos("sequence", current_beam->par_names);
-  if (nl->inform[pos])
-  {
+
+  if (nl->inform[pos]) {
     name = pl->parameters[pos]->string;
-    if ((current_beam = find_command(name, beam_list)) == NULL)
-    {
-      set_defaults("beam");
+    if ((current_beam = find_command(name, beam_list)) == NULL) {
+      set_defaults("beam"); 
       add_to_command_list(name, current_beam, beam_list, 0);
     }
   }
-  else
-  {
+  else {
     name = name_def;
     current_beam = find_command(name, beam_list);
   }
   current_beam->par->parameters[bpos]->string = permbuff(name);
   current_beam->beam_def = 1;
-  if (flag == 0) update_beam(cmd->clone);
-  else if (flag == 1)  set_defaults("beam");
+  if (flag == 0) update_beam(cmd->clone); 
+  else if (flag == 1)  set_defaults("beam"); // resbeam
   current_beam = keep_beam;
 }
 
@@ -119,174 +117,156 @@ update_beam(struct command* comm)
   int pos, lp;
   char* name = blank;
   double energy = 0, beta = 0, gamma = 0, charge = 0, freq0 = 0, bcurrent = 0,
-    npart = 0, mass = 0, pc = 0, ex, exn, ey, eyn, alfa, circ = 0,
-    arad = 0;
+    npart = 0, mass = 0, pc = 0, circ = 0, arad = 0,
+    ex, exn, ey, eyn, alfa;
 
   pos = name_list_pos("particle", nlc);
-  if (nlc->inform[pos])  /* parameter has been read */
-    {
-      pl->parameters[pos]->string = name
-	= plc->parameters[pos]->string;
-      if ((lp = name_list_pos(name, defined_commands->list)) > -1)
-	{
-	  mass = command_par_value("mass", defined_commands->commands[lp]);
-	  charge = command_par_value("charge", defined_commands->commands[lp]);
-	}
-      else /* unknown particle, then mass and charge must be given as well */
-	{
-	  pos = name_list_pos("mass", nlc);
-	  if (nlc->inform[pos]) mass = command_par_value("mass", comm);
-	  else
-	    {
-	      warning("emass given to unknown particle:", name);
-	      mass = get_variable("emass");
-	    }
-	  pos = name_list_pos("charge", nlc);
-	  if (nlc->inform[pos]) charge = command_par_value("charge", comm);
-	  else
-	    {
-	      warning("charge +1 given to unknown particle:", name);
-	      charge = 1;
-	    }
-	}
+  if (nlc->inform[pos]) { /* parameter has been read */
+    pl->parameters[pos]->string = name = plc->parameters[pos]->string;
+    if ((lp = name_list_pos(name, defined_commands->list)) > -1) { // known particle
+      mass = command_par_value("mass", defined_commands->commands[lp]);
+      charge = command_par_value("charge", defined_commands->commands[lp]);
     }
-  else if (nlc->inform[name_list_pos("mass", nlc)])
-    {
-      mass = command_par_value("mass", comm);
-      pl->parameters[pos]->string = name = permbuff("default");
-      pos = name_list_pos("charge", nlc);
-      if (nlc->inform[pos]) charge = command_par_value("charge", comm);
-      else
-	{
-	  warning("charge +1 given to user particle:", name);
-	  charge = 1;
-	}
-    }
-  else name = pl->parameters[pos]->string;
-
-  if (strcmp(name, "ion") == 0)
-    {
+    else { /* unknown particle, then mass and charge must be given as well */
       pos = name_list_pos("mass", nlc);
       if (nlc->inform[pos]) mass = command_par_value("mass", comm);
+      else { // default is emass
+	warning("emass given to unknown particle:", name);
+	mass = get_variable("emass");
+      }
       pos = name_list_pos("charge", nlc);
       if (nlc->inform[pos]) charge = command_par_value("charge", comm);
-      else charge = command_par_value("charge", current_beam);
+      else { //default is charge +1
+	warning("charge +1 given to unknown particle:", name);
+	charge = 1;
+      }
     }
+  }
+  else if (nlc->inform[name_list_pos("mass", nlc)]) {
+    mass = command_par_value("mass", comm);
+    pl->parameters[pos]->string = name = permbuff("default");
+    pos = name_list_pos("charge", nlc);
+    if (nlc->inform[pos]) charge = command_par_value("charge", comm);
+    else {
+      warning("charge +1 given to user particle:", name);
+      charge = 1;
+    }
+  }
+  else name = pl->parameters[pos]->string; 
+
+  if (strcmp(name, "ion") == 0) {
+    pos = name_list_pos("mass", nlc);
+    if (nlc->inform[pos]) mass = command_par_value("mass", comm);
+    pos = name_list_pos("charge", nlc);
+    if (nlc->inform[pos]) charge = command_par_value("charge", comm);
+    else charge = command_par_value("charge", current_beam);
+  }
 
   if (mass == zero) mass = command_par_value("mass", current_beam);
 
   if (charge == zero) charge = command_par_value("charge", current_beam);
 
-  arad = ten_m_16 * charge * charge * get_variable("qelect")
-    * clight * clight / mass;
+  arad = ten_m_16 * charge * charge * get_variable("qelect") * clight * clight / mass;
 
-  if ((pos = name_list_pos("energy", nlc)) > -1 && nlc->inform[pos])
-    {
-      energy = command_par_value("energy", comm);
-      if (energy <= mass) fatal_error("energy must be","> mass");
-      pc = sqrt(energy*energy - mass*mass);
-      gamma = energy / mass;
-      beta = pc / energy;
-    }
-  else if((pos = name_list_pos("pc", nlc)) > -1 && nlc->inform[pos])
-    {
-      pc = command_par_value("pc", comm);
-      energy = sqrt(pc*pc + mass*mass);
-      gamma = energy / mass;
-      beta = pc / energy;
-    }
-  else if((pos = name_list_pos("gamma", nlc)) > -1 && nlc->inform[pos])
-    {
-      if ((gamma = command_par_value("gamma", comm)) <= one)
-	fatal_error("gamma must be","> 1");
-      energy = gamma * mass;
-      pc = sqrt(energy*energy - mass*mass);
-      beta = pc / energy;
-    }
-  else if((pos = name_list_pos("beta", nlc)) > -1 && nlc->inform[pos])
-    {
-      if ((beta = command_par_value("beta", comm)) >= one)
-	fatal_error("beta must be","< 1");
-      gamma = one / sqrt(one - beta*beta);
-      energy = gamma * mass;
-      pc = sqrt(energy*energy - mass*mass);
-    }
-  else
-    {
-      energy = command_par_value("energy", current_beam);
-      if (energy <= mass) fatal_error("energy must be","> mass");
-      pc = sqrt(energy*energy - mass*mass);
-      gamma = energy / mass;
-      beta = pc / energy;
-    }
+  // energy related 
+  if ((pos = name_list_pos("energy", nlc)) > -1 && nlc->inform[pos]) {
+    energy = command_par_value("energy", comm);
+    if (energy <= mass) fatal_error("energy must be","> mass");
+    pc = sqrt(energy*energy - mass*mass);
+    gamma = energy / mass;
+    beta = pc / energy;
+  }
+  else if((pos = name_list_pos("pc", nlc)) > -1 && nlc->inform[pos]) {
+    pc = command_par_value("pc", comm);
+    energy = sqrt(pc*pc + mass*mass);
+    gamma = energy / mass;
+    beta = pc / energy;
+  }
+  else if((pos = name_list_pos("gamma", nlc)) > -1 && nlc->inform[pos]) {
+    if ((gamma = command_par_value("gamma", comm)) <= one) fatal_error("gamma must be","> 1");
+    energy = gamma * mass;
+    pc = sqrt(energy*energy - mass*mass);
+    beta = pc / energy;
+  }
+  else if((pos = name_list_pos("beta", nlc)) > -1 && nlc->inform[pos]) {
+    if ((beta = command_par_value("beta", comm)) >= one) fatal_error("beta must be","< 1");
+    gamma = one / sqrt(one - beta*beta);
+    energy = gamma * mass;
+    pc = sqrt(energy*energy - mass*mass);
+  }
+  else {
+    energy = command_par_value("energy", current_beam);
+    if (energy <= mass) fatal_error("energy must be","> mass");
+    pc = sqrt(energy*energy - mass*mass);
+    gamma = energy / mass;
+    beta = pc / energy;
+  }
 
-  if (nlc->inform[name_list_pos("ex", nlc)])
-    {
-      ex = command_par_value("ex", comm);
-      exn = ex * 4 * beta * gamma;
-    }
-  else if (nlc->inform[name_list_pos("exn", nlc)])
-    {
-      exn = command_par_value("exn", comm);
-      ex = exn / (4 * beta * gamma);
-    }
-  else
-    {
-      ex = command_par_value("ex", current_beam);
-      exn = ex * 4 * beta * gamma;
-    }
+  // emittance related
+  if (nlc->inform[name_list_pos("ex", nlc)]) {
+    ex = command_par_value("ex", comm);
+    exn = ex * 4 * beta * gamma;
+  }
+  else if (nlc->inform[name_list_pos("exn", nlc)]) {
+    exn = command_par_value("exn", comm);
+    ex = exn / (4 * beta * gamma);
+  }
+  else {
+    ex = command_par_value("ex", current_beam);
+    exn = ex * 4 * beta * gamma;
+  }
 
-  if (nlc->inform[name_list_pos("ey", nlc)])
-    {
-      ey = command_par_value("ey", comm);
-      eyn = ey * 4 * beta * gamma;
-    }
-  else if (nlc->inform[name_list_pos("eyn", nlc)])
-    {
-      eyn = command_par_value("eyn", comm);
-      ey = eyn / (4 * beta * gamma);
-    }
-  else
-    {
-      ey = command_par_value("ey", current_beam);
-      eyn = ey * 4 * beta * gamma;
-    }
+  if (nlc->inform[name_list_pos("ey", nlc)]) {
+    ey = command_par_value("ey", comm);
+    eyn = ey * 4 * beta * gamma;
+  }
+  else if (nlc->inform[name_list_pos("eyn", nlc)]) {
+    eyn = command_par_value("eyn", comm);
+    ey = eyn / (4 * beta * gamma);
+  }
+  else {
+    ey = command_par_value("ey", current_beam);
+    eyn = ey * 4 * beta * gamma;
+  }
 
   alfa = one / (gamma * gamma);
 
-  if (nlc->inform[name_list_pos("circ", nlc)])
-    {
-      circ = command_par_value("circ", comm);
-      if (circ > zero) freq0 = (beta * clight) / (ten_p_6 * circ);
-    }
-  else if (nlc->inform[name_list_pos("freq0", nlc)])
-    {
-      freq0 = command_par_value("freq0", comm);
-      if (freq0 > zero) circ = (beta * clight) / (ten_p_6 * freq0);
-    }
-  else if ((pos = name_list_pos(name, sequences->list)) >= 0)
-    {
-      circ = sequence_length(sequences->sequs[pos]);
-      freq0 = (beta * clight) / (ten_p_6 * circ);
-    }
-
-  if (nlc->inform[name_list_pos("bcurrent", nlc)])
-    {
-      bcurrent = command_par_value("bcurrent", comm);
-      if (bcurrent > zero && freq0 > zero)
-	npart = bcurrent / (beta * freq0 * ten_p_6 * get_variable("qelect"));
-      else if (nlc->inform[name_list_pos("npart", nlc)])
-	{
-	  npart = command_par_value("npart", comm);
-	  bcurrent = npart * beta * freq0 * ten_p_6 * get_variable("qelect");
-	}
-    }
-
-  else if (nlc->inform[name_list_pos("npart", nlc)])
-    {
+  // circumference related
+  if (nlc->inform[name_list_pos("circ", nlc)]) {
+    circ = command_par_value("circ", comm);
+    if (circ > zero) freq0 = (beta * clight) / (ten_p_6 * circ);
+    // 2014-Apr-09  15:45:51  ghislain: update freq0 and circ only as necessary
+    store_comm_par_value("freq0", freq0, current_beam);
+    store_comm_par_value("circ", circ, current_beam);
+  }
+  else if (nlc->inform[name_list_pos("freq0", nlc)]) {
+    freq0 = command_par_value("freq0", comm);
+    if (freq0 > zero) circ = (beta * clight) / (ten_p_6 * freq0);
+    // 2014-Apr-09  15:45:51  ghislain: update freq0 and circ only as necessary
+    store_comm_par_value("freq0", freq0, current_beam);
+    store_comm_par_value("circ", circ, current_beam);
+  }
+  // 2014-Apr-09  15:44:05  ghislain: name contains the particle name if any so this
+  // attempt to get circumference from sequence length fails always...
+  /*else if ((pos = name_list_pos(name, sequences->list)) >= 0) { 
+    circ = sequence_length(sequences->sequs[pos]);
+    freq0 = (beta * clight) / (ten_p_6 * circ);
+  } */
+  
+  // intensity related
+  if (nlc->inform[name_list_pos("bcurrent", nlc)]) {
+    bcurrent = command_par_value("bcurrent", comm);
+    if (bcurrent > zero && freq0 > zero)
+      npart = bcurrent / (beta * freq0 * ten_p_6 * get_variable("qelect"));
+    else if (nlc->inform[name_list_pos("npart", nlc)]) {
       npart = command_par_value("npart", comm);
       bcurrent = npart * beta * freq0 * ten_p_6 * get_variable("qelect");
     }
+  }
+  else if (nlc->inform[name_list_pos("npart", nlc)]) {
+    npart = command_par_value("npart", comm);
+    bcurrent = npart * beta * freq0 * ten_p_6 * get_variable("qelect");
+  }
 
   pos = name_list_pos("bunched", nlc);
   if (nlc->inform[pos])
@@ -331,8 +311,9 @@ update_beam(struct command* comm)
   store_comm_par_value("eyn", eyn, current_beam);
   store_comm_par_value("npart", npart, current_beam);
   store_comm_par_value("bcurrent", bcurrent, current_beam);
-  store_comm_par_value("freq0", freq0, current_beam);
-  store_comm_par_value("circ", circ, current_beam);
+  // 2014-Apr-09  15:44:57  ghislain: update of freq0 and circ is done above
+  //store_comm_par_value("freq0", freq0, current_beam);
+  //store_comm_par_value("circ", circ, current_beam);
   store_comm_par_value("beta", beta, current_beam);
   store_comm_par_value("alfa", alfa, current_beam);
   store_comm_par_value("arad", arad, current_beam);
@@ -344,12 +325,16 @@ adjust_beam(void)
 {
   struct name_list* nl = current_beam->par_names;
   double circ = one, freq0, alfa, beta, gamma, bcurrent = zero, npart = 0;
+
   if (current_sequ != NULL && sequence_length(current_sequ) != zero)
     circ = current_sequ->length;
+
   beta = command_par_value("beta", current_beam);
   gamma = command_par_value("gamma", current_beam);
   alfa = one / (gamma * gamma);
+
   freq0 = (beta * clight) / (ten_p_6 * circ);
+
   if (nl->inform[name_list_pos("bcurrent", nl)] &&
       (bcurrent = command_par_value("bcurrent", current_beam)) > zero)
     npart = bcurrent / (freq0 * ten_p_6 * get_variable("qelect"));
