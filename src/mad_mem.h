670492d27d1028555bbcdc74e319579b993454d0
Laurent Deniau <Laurent.Deniau@cern.ch>
2014-06-30 06:42:39 +0000

    r4865: maximum memory collection enabled to see if it avoids memory leaks on 32 bits arch

--- src/mad_mem.h
+++ src/mad_mem.h
@@ -10,11 +10,13 @@
 #define mymalloc(fn, sz)            myptrchk(fn, GC_MALLOC_IGNORE_OFF_PAGE(sz))
 #define mymalloc_atomic(fn, sz)     myptrchk(fn, GC_MALLOC_ATOMIC_IGNORE_OFF_PAGE(sz))
 #define myrealloc(fn, p, sz)        myptrchk(fn, GC_REALLOC((p),(sz)))
-#define myfree(fn, p)               ((void)((void)fn, (p)=0))
+#define myfree(fn, p)               ((void)(GC_FREE(p), (void)fn, (p)=0))
 
 #define mycalloc(fn, n, sz)         memset(mymalloc(fn, (n)*(sz)), 0, (n)*(sz))
 #define mycalloc_atomic(fn, n, sz)  memset(mymalloc_atomic(fn, (n)*(sz)), 0, (n)*(sz))
 #define myrecalloc(fn, p, osz, sz)  ((void*)((char*)memset((char*)myptrchk(fn,GC_REALLOC((p),(sz)))+(osz),0,(sz)-(osz))-(osz)))
+
+#define mycollect()                 GC_gcollect()
 
 #else
 
@@ -26,6 +28,8 @@
 #define mycalloc(fn, n, sz)         myptrchk(fn, calloc((n),(sz)))
 #define mycalloc_atomic(fn, n, sz)  myptrchk(fn, calloc((n),(sz)))
 #define myrecalloc(fn, p, osz, sz)  ((void*)((char*)memset((char*)myptrchk(fn,realloc((p),(sz)))+(osz),0,(sz)-(osz))-(osz)))
+
+#define mycollect()                 
 
 #endif
 
