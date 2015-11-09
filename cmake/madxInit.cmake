5f5c3476a5a1d8dbdf4a39fc506587af1f8c6dc2
Yngve Inntjore Levinsen <yngve.levinsen@gmail.com>
2015-11-09 08:54:21 +0000

    r5512: added _VERSION_NUM to cmake defines

--- cmake/madxInit.cmake
+++ cmake/madxInit.cmake
@@ -24,11 +24,15 @@ string(REGEX REPLACE "VERSION *= *" "" MADX_VERSION ${MADX_VERSION})
 string(REGEX REPLACE "^([0-9])+.[0-9]+.[0-9]+" "\\1" MADX_MAJOR_VERSION ${MADX_VERSION})
 string(REGEX REPLACE "^[0-9]+.([0-9]+).[0-9]+" "\\1" MADX_MINOR_VERSION ${MADX_VERSION})
 string(REGEX REPLACE "^[0-9]+.[0-9]+.([0-9]+)" "\\1" MADX_PATCH_LEVEL ${MADX_VERSION})
+#VERSION_NUM:
+string(REGEX MATCH "VERSION_NUM += *[^\n]+" VERSION_NUM  ${VERSION_INFO})
+string(REGEX REPLACE "VERSION_NUM += *" "" VERSION_NUM  ${VERSION_NUM})
 #VERSION_DATE:
 string(REGEX MATCH "VERSION_DATE += *[^\n]+" VERSION_DATE  ${VERSION_INFO})
 string(REGEX REPLACE "VERSION_DATE += *" "" VERSION_DATE  ${VERSION_DATE})
 
 message(STATUS "Mad-X version: ${MADX_VERSION}")
+message(STATUS "Version num: ${VERSION_NUM}")
 message(STATUS "Version date: ${VERSION_DATE}")
 
 # Append _dev/-dev to binary/package name
