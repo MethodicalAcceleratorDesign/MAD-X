a071e9e461dc494d8b87429fa4d1491d835395c8
Laurent Deniau <Laurent.Deniau@cern.ch>
2016-02-16 17:22:48 +0000

    r5678: rfc print moved to beam module + some cleaning

--- src/mad_emit.c
+++ src/mad_emit.c
@@ -1,41 +1,14 @@
 #include "madx.h"
 
-static void
-print_rfc(void)
-  /* prints the rf cavities present */
-{
-  double freq0, harmon, freq;
-  int i, n = current_sequ->cavities->curr;
-  struct element* el;
-  if (n == 0)  return;
-  freq0 = command_par_value("freq0", probe_beam);
-  printf("\n RF system: \n");
-  printf(v_format(" %S %NFs %NFs %NFs %NFs %NFs\n"),
-         "Cavity","length[m]","voltage[MV]","lag","freq[MHz]","harmon");
-  for (i = 0; i < n; i++)
-  {
-    el = current_sequ->cavities->elem[i];
-    if ((harmon = el_par_value("harmon", el)) > zero)
-    {
-      freq = freq0 * harmon;
-      printf(v_format(" %S %F %F %F %F %F\n"),
-             el->name, el->length, el_par_value("volt", el),
-             el_par_value("lag", el), freq, harmon);
-    }
-  }
-}
-
 // public interface
 
 void
 pro_emit(struct in_cmd* cmd)
   /* calls the emit module */
 {
-  const char *rout_name = "pro_emit";
   struct command* emit = cmd->clone;
   double e_deltap, e_tol, u0;
   int j, error, keep;
-  double* tt;
   double emit_v[3], nemit_v[3], bmax[9], gmax[9], dismax[4], tunes[3],
     sig_v[4], pdamp[3], r0mat[4];
   char tmp[100];
@@ -60,10 +33,8 @@ pro_emit(struct in_cmd* cmd)
 
   zero_double(orbit0, 6);
   zero_double(disp0, 6);
-  zero_double(oneturnmat, 36);
+  zero_double(oneturnmat, 6*6);
 
-  tt = mycalloc_atomic("pro_emit", 216, sizeof *tt);
-  
   adjust_beam();
   probe_beam = clone_command(current_beam);
 
@@ -71,15 +42,14 @@ pro_emit(struct in_cmd* cmd)
   twcpin_(oneturnmat,disp0,r0mat,&error); /* added for disp0 computation */
 
   adjust_probe(e_deltap); /* sets correct gamma, beta, etc. */
-  print_global(e_deltap);
+  print_probe();
   adjust_rfc(); /* sets freq in rf-cavities from probe */
 
   // guess_flag is set by COGUESS command
   if (guess_flag) copy_double(guess_orbit, orbit0, 6);
 
+  double tt[6*6*6];
   getclor_(orbit0, oneturnmat, tt, &error); /* closed orbit */
-
-  myfree(rout_name, tt);
 
   if (error == 0) {
     current_node = current_sequ->ex_start;
