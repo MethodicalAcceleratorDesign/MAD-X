039ab885758b54a2597ee36ae77c005daed09241
Laurent Deniau <Laurent.Deniau@cern.ch>
2016-02-18 13:15:41 +0000

    r5699: cleaning+bug fix in comments

--- src/mad_track.c
+++ src/mad_track.c
@@ -20,18 +20,31 @@ track_observe(struct in_cmd* cmd)
     nodes[0]->obs_point = ++curr_obs_points;
     nodes[0]->obs_orbit = new_double_array(6);
     nodes[0]->obs_orbit->curr = 6;
+
+    // LD 2016.02.18: START
+    // zero_double(orbit0, 6);
+    /* zero_double(disp0, 6); */
+    // zero_double(oneturnmat, 6*6);
+
     adjust_beam();
-    if (probe_beam) probe_beam = delete_command(probe_beam);
     probe_beam = clone_command(current_beam);
+    // adjust_rfc(); /* sets freq in rf-cavities from probe */
+
+    // LD 2016.02.17: BUG, depends on the previous oneturnmap and disp0
     adjust_probe(track_deltap); /* sets correct gamma, beta, etc. */
     adjust_rfc(); /* sets freq in rf-cavities from probe */
-    zero_double(orbit0, 6);
-    zero_double(oneturnmat, 36);
+
+    zero_double(orbit0, 6); // should be moved up
+    zero_double(oneturnmat, 6*6); // should be moved up
+    // LD 2016.02.18: START
+
     if (get_option("onepass") == 0)
     {
       tmrefo_(&curr_obs_points,orbit0,nodes[0]->obs_orbit->a,oneturnmat);
       /* closed orbit and one-turn linear transfer map */
     }
+
+    probe_beam = delete_command(probe_beam); // LD: added...
   }
   else
   {
@@ -62,13 +75,20 @@ track_run(struct in_cmd* cmd)
     return;
   }
 
+  // LD 2016.02.18: START
   adjust_beam();
-  if (probe_beam) probe_beam = delete_command(probe_beam);
   probe_beam = clone_command(current_beam);
+  // adjust_rfc(); /* sets freq in rf-cavities from probe */
+
+  // LD 2016.02.17: BUG, depends on the previous oneturnmap and disp0
   adjust_probe(track_deltap); /* sets correct gamma, beta, etc. */
   adjust_rfc(); /* sets freq in rf-cavities from probe */
+
+  // LD: to move before the adjust_probe ?
   zero_double(orbit0, 6);
-  zero_double(oneturnmat, 36);
+  // zero_double(disp0, 6);
+  zero_double(oneturnmat, 6*6);
+  // LD 2016.02.18: END
 
   if (get_option("onepass") == 0) {
     tmrefo_(&izero,orbit0,orbit,oneturnmat);
@@ -100,6 +120,8 @@ track_run(struct in_cmd* cmd)
   t = table_register->tables[name_list_pos("tracksumm", table_register->names)];
   if (get_option("info")) print_table(t);
   if (get_option("track_dump")) track_tables_dump();
+
+  probe_beam = delete_command(probe_beam); // LD: added...
 
   /* free buffers */
   myfree(rout_name, ibuf1);   myfree(rout_name, ibuf2);  myfree(rout_name, ibuf3);
