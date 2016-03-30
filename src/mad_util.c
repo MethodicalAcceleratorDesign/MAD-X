7e2d66c06f54874cac6d0d70ec09d6e7839f2f1b
mad <mad@cern.ch>
2016-03-30 14:10:57 +0000

    r5744: better detection of interactive mode (was failing on msys)

--- src/mad_util.c
+++ src/mad_util.c
@@ -1,25 +1,30 @@
 #define _POSIX_C_SOURCE 200112L
 #include "madx.h"
 
-#ifndef _WIN32
+//#ifndef _WIN32
 #include <unistd.h>
+#include <sys/stat.h>
 
 int
 intrac(void)
 {
   /* returns non-zero if program is used interactively, else 0 */
-  return isatty(fileno(in->input_files[0]));
+  if (in->input_files[0] != stdin) return 0;
+
+  struct stat stats;
+  fstat(0, &stats);
+  return (in->input_files[0] == stdin && S_ISFIFO(stats.st_mode)) || isatty(0);
 }
 
-#else // _WIN32
-#include <io.h>
-
-int
-intrac(void)
-{
-  /* returns non-zero if program is used interactively, else 0 */
-  return _isatty(_fileno(in->input_files[0]));
-}
-
-#endif
+//#else // _WIN32
+//#include <io.h>
+//
+//int
+//intrac(void)
+//{
+//  /* returns non-zero if program is used interactively, else 0 */
+//  return _isatty(_fileno(in->input_files[0]));
+//}
+//
+//#endif
 
