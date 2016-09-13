c8f5df3aabc97e2afb7c0b83931405abb0535778
Yngve Inntjore Levinsen <yngve.levinsen@gmail.com>
2016-09-13 14:25:08 +0000

    r5933: cmake: silly fix to find gnu fortran compiler versions > 5

--- cmake/compilers/setupGNU.cmake
+++ cmake/compilers/setupGNU.cmake
@@ -9,7 +9,7 @@ if (CMAKE_Fortran_COMPILER_ID MATCHES "GNU")
     # General:
     set(CMAKE_Fortran_FLAGS " -fno-range-check -fno-f2c -cpp ") # remove -g -O2 from main list
     execute_process(COMMAND ${CMAKE_Fortran_COMPILER} --version OUTPUT_VARIABLE CMAKE_Fortran_COMPILER_VERSION)
-    string(REGEX MATCH "[3-5].[0-9].[0-9]" CMAKE_Fortran_COMPILER_VERSION ${CMAKE_Fortran_COMPILER_VERSION})
+    string(REGEX MATCH "[3-9].[0-9].[0-9]" CMAKE_Fortran_COMPILER_VERSION ${CMAKE_Fortran_COMPILER_VERSION})
     if(${CMAKE_Fortran_COMPILER_VERSION} VERSION_GREATER 4.3.9)
         add_definitions(-D_GFORTRAN)
     endif()
