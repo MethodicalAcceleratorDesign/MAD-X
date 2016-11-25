f74a21620a7ff6b36dfc71f43627b2b5fd4b704f
Yngve Inntjore Levinsen <yngve.levinsen@gmail.com>
2016-11-25 10:12:52 +0000

    r6015: [cmake edits]
     - error in ndiff, include(CTest) should not be used if CTestConfig is not defined for project
     - option to include ndiff in installation
     - install destination of emacs syntax script changed
     - removing double comment quotes

--- CTestConfig.cmake
+++ CTestConfig.cmake
@@ -1,9 +1,9 @@
-## This file should be placed in the root directory of your project.
-## Then modify the CMakeLists.txt file in the root directory of your
-## project to incorporate the testing dashboard.
-## # The following are required to uses Dart and the Cdash dashboard
-##   ENABLE_TESTING()
-##   INCLUDE(CTest)
+# This file should be placed in the root directory of your project.
+# Then modify the CMakeLists.txt file in the root directory of your
+# project to incorporate the testing dashboard.
+# # The following are required to uses Dart and the Cdash dashboard
+#   ENABLE_TESTING()
+#   INCLUDE(CTest)
 set(CTEST_PROJECT_NAME "Mad-X")
 set(CTEST_NIGHTLY_START_TIME "00:00:00 CET")
 set(CTEST_DROP_METHOD "http")
