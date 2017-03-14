cd51e39491aa94c0cef60c5625c4221e68b6e2e2
Riccardo De Maria <Riccardo.De.Maria@cern.ch>
2017-03-14 11:32:03 +0000

    r6151: Adding `append option` to `setvars_knob` command

--- src/mad_exec.c
+++ src/mad_exec.c
@@ -652,6 +652,7 @@ exec_setvars_knob_table(struct in_cmd* cmd)
   char expr[10000];
   char subexpr[100];
   int pos, row;
+  int append;
 
   if ((pos = name_list_pos("table", nl)) < 0 || nl->inform[pos] == 0 ||
       (name = pl->parameters[pos]->string) == NULL) {
@@ -670,6 +671,8 @@ exec_setvars_knob_table(struct in_cmd* cmd)
   row  = pos >= 0 ? (int) pl->parameters[pos]->double_value : t->curr;
   pos  = name_list_pos("knob", nl);
   knob = pos >= 0 ? pl->parameters[pos]->string : NULL;
+  pos  = name_list_pos("append",nl);
+  append= pos >= 0 ? pl->parameters[pos]->double_value : 1;
 
   if (abs(row) > t->curr || row == 0) {
     warning("row index out of bounds:", " ignored");
@@ -693,7 +696,7 @@ exec_setvars_knob_table(struct in_cmd* cmd)
       const char *colname = t->columns->names[i];
       double val = t->d_cols[i][row-1];
       sprintf(subexpr,"%+20.16g*%s", val, knob);
-      if ((var = find_variable(colname, variable_list))) {
+      if (append && (var = find_variable(colname, variable_list))) {
         if (var->expr)
           sprintf(expr, "%s := %s %s;", colname, var->expr->string, subexpr);
         else
