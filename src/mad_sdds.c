e6935cc50635aa8c8135668e6a101cc9c32f5b95
Laurent Deniau <laurent.deniau@gmail.com>
2017-10-26 14:12:42 +0200

    fix uninitialized variable

--- src/mad_sdds.c
+++ src/mad_sdds.c
@@ -24,7 +24,7 @@ head_split(char* buf, struct char_p_array* list)
       replace(list->p[j], '@', ' ');
   return list->curr;
 }
-                                                                                                     
+
 static int
 sdds_get_parm(SDDS_TABLE *SDDS_table, struct table *tfs_table)
 {
@@ -128,15 +128,15 @@ sdds_readt(char *filename, char *tfsname)
        }
   }
 
-  /* open the file and read the SDDS header */ 
+  /* open the file and read the SDDS header */
   if(SDDS_InitializeInput(&SDDS_table, filename) != 1) {
      SDDS_PrintErrors(stdout,SDDS_VERBOSE_PrintErrors);
   }
 
-  /* read and process each data table in the data set */ 
+  /* read and process each data table in the data set */
   while (SDDS_ReadTable(&SDDS_table) > 0) { // (lb = not used
-      /* set all rows and all columns to initially be "of interest" */ 
-      SDDS_SetColumnFlags(&SDDS_table, 1); 
+      /* set all rows and all columns to initially be "of interest" */
+      SDDS_SetColumnFlags(&SDDS_table, 1);
       SDDS_SetRowFlags(&SDDS_table, 1);
 
   }
@@ -153,7 +153,7 @@ sdds_readt(char *filename, char *tfsname)
 
       narr = 0;
 
-        for(i1=0;i1<nall;i1++){ 
+        for(i1=0;i1<nall;i1++){
            if(sdds_pat != NULL) {
              if (get_option("debug")) printf("Check %d patterns\n",sdds_pat->curr);
              for(i2=0; i2 < sdds_pat->curr; i2++) {
@@ -170,23 +170,23 @@ sdds_readt(char *filename, char *tfsname)
            }
         }
 
-      for(i1=0;i1<narr;i1++){ 
-        if ((SDDS_CheckArray(&SDDS_table, c1[i1], NULL, 
+      for(i1=0;i1<narr;i1++){
+        if ((SDDS_CheckArray(&SDDS_table, c1[i1], NULL,
                       0, stderr)) != SDDS_CHECK_OKAY) {
-           fprintf(stderr, "array %s is not in the data file",c1[i1]); 
-           exit(1); 
-        } 
+           fprintf(stderr, "array %s is not in the data file",c1[i1]);
+           exit(1);
+        }
 
         arr = SDDS_GetArray(&SDDS_table, c1[i1], NULL);
         arrdim[i1] = (long)arr->definition->dimensions;
         arrtyp[i1] = (long)arr->definition->type;
-        arrele[i1] = (long)arr->elements;          
-                                                                                                            
+        arrele[i1] = (long)arr->elements;
+
   /* check whether type is valid and compatible with TFS */
         if((arrtyp[i1] < 1) || (arrtyp[i1] > SDDS_STRING))     {
            fatal_error("Type not valid or compatible for: ",c1[i1]);
         }
-                                                                                                            
+
   /* check whether array dimension and size is compatible with TFS */
         if(arrdim[i1] != 1 )     {
            fatal_error("Array is 2-Dimensional: ",c1[i1]);
@@ -203,20 +203,20 @@ sdds_readt(char *filename, char *tfsname)
         if(arrtyp[i1] == SDDS_STRING) datstr[i1] = (char **)arr->data;
 
         for(i3=0;i3<arr->elements;i3++){
-           datmp = (double *)arr->data;   
-           datmpl = (long *)arr->data;   
-           datmps = (short *)arr->data;   
-           datmpf = (float *)arr->data;   
+           datmp = (double *)arr->data;
+           datmpl = (long *)arr->data;
+           datmps = (short *)arr->data;
+           datmpf = (float *)arr->data;
            if (get_option("debug")) {
              if(arrtyp[i1] != SDDS_STRING) printf("data: %e %e \n",(double)datmp[i3],datmp[i3]);
              if(arrtyp[i1] == SDDS_STRING) printf("data: %s \n",datstr[i1][i3]);
            }
-           if(arrtyp[i1] == SDDS_DOUBLE) datd[i1][i3] = (double)datmp[i3];     
+           if(arrtyp[i1] == SDDS_DOUBLE) datd[i1][i3] = (double)datmp[i3];
            /* for float data: convert to double float */
-           if(arrtyp[i1] == SDDS_FLOAT)  datd[i1][i3] = datmpf[i3];     
+           if(arrtyp[i1] == SDDS_FLOAT)  datd[i1][i3] = datmpf[i3];
            /* for integer data: convert to double float */
-           if(arrtyp[i1] == SDDS_LONG)   datd[i1][i3] = datmpl[i3];     
-           if(arrtyp[i1] == SDDS_SHORT)  datd[i1][i3] = datmps[i3];     
+           if(arrtyp[i1] == SDDS_LONG)   datd[i1][i3] = datmpl[i3];
+           if(arrtyp[i1] == SDDS_SHORT)  datd[i1][i3] = datmps[i3];
         }
         if (get_option("debug")) {
            printf("For TFS table: %s %ld %ld %d\n",c1[i1],arrdim[i1],arrtyp[i1],tfs_sdds_types[arrtyp[i1]]);
@@ -238,7 +238,7 @@ sdds_readt(char *filename, char *tfsname)
        tfs_table_cols[3] = c1[3];
        strcpy(&tfs_table_cols[narr]," ");
   */
-        
+
        tfstab = make_table(tfsname, tfsname, (const char* const*)tfs_table_cols, tfs_table_types, 5000);
        add_to_table_list(tfstab, table_register);
 
@@ -266,7 +266,7 @@ sdds_readt(char *filename, char *tfsname)
   */
                    augment_count(tfsname);
            }
-                                                                                                            
+
 
 
        if (get_option("debug")) printf("--> %d %d\n",tfstab->curr,tfstab->num_cols);
@@ -309,14 +309,14 @@ treat_tfs_header_set(SDDS_TABLE *SDDS_table, struct table* t)
 
         if(strcmp(head_buf->p[2],"%ld") == 0) {
           sscanf(head_buf->p[3],"%ld",&lbuf);
-          if (!SDDS_SetParameters(SDDS_table, SDDS_SET_BY_NAME|SDDS_PASS_BY_VALUE, 
+          if (!SDDS_SetParameters(SDDS_table, SDDS_SET_BY_NAME|SDDS_PASS_BY_VALUE,
                                head_buf->p[1], lbuf, NULL)) {
              SDDS_PrintErrors(stderr, SDDS_VERBOSE_PrintErrors);
              exit(1);
           }
         } else if(strcmp(head_buf->p[2],"%le") == 0) {
           sscanf(head_buf->p[3],"%le",&dbuf);
-          if (!SDDS_SetParameters(SDDS_table, SDDS_SET_BY_NAME|SDDS_PASS_BY_VALUE, 
+          if (!SDDS_SetParameters(SDDS_table, SDDS_SET_BY_NAME|SDDS_PASS_BY_VALUE,
                                head_buf->p[1], dbuf, NULL)) {
              SDDS_PrintErrors(stderr, SDDS_VERBOSE_PrintErrors);
              exit(1);
@@ -324,7 +324,7 @@ treat_tfs_header_set(SDDS_TABLE *SDDS_table, struct table* t)
         } else  {
           strcpy(dumc,head_buf->p[3]);
           replace(dumc, '\"', ' ');
-          if (!SDDS_SetParameters(SDDS_table, SDDS_SET_BY_NAME|SDDS_PASS_BY_VALUE,     
+          if (!SDDS_SetParameters(SDDS_table, SDDS_SET_BY_NAME|SDDS_PASS_BY_VALUE,
                                head_buf->p[1], dumc, NULL)) {
              SDDS_PrintErrors(stderr, SDDS_VERBOSE_PrintErrors);
              exit(1);
@@ -350,7 +350,7 @@ treat_tfs_header_define(SDDS_TABLE *SDDS_table, struct table* t)
   for(j=0; j < t->header->curr; j++) {
     if (get_option("debug")) printf("header: %s\n", t->header->p[j]);
     pre_split(t->header->p[j], l_wrk, 0);
-    head_split(l_wrk->c,head_buf); // i = not used 
+    head_split(l_wrk->c,head_buf); // i = not used
     if (get_option("debug")) printf("curr: %d\n",head_buf->curr);
 
     if(head_buf->curr > 0) {
@@ -371,7 +371,7 @@ treat_tfs_header_define(SDDS_TABLE *SDDS_table, struct table* t)
 
   return(head_buf->curr);
 }
-                                                                                                    
+
 static int
 sdds_writet_sel(char *filename, struct table *tfstab)
 {
@@ -395,15 +395,15 @@ sdds_writet_sel(char *filename, struct table *tfstab)
   {
     -1, SDDS_LONG, SDDS_DOUBLE, SDDS_STRING
   };
-                                                                                                            
+
   /* set up to put data into file "atest.out" */
     if (!SDDS_InitializeOutput(&SDDS_table, SDDS_ASCII, 1, NULL, NULL, filename)) {
       SDDS_PrintErrors(stderr, SDDS_VERBOSE_PrintErrors);
       exit(1);
     }
-     
 
-  /*     */ 
+
+  /*     */
   if (get_option("debug")) {
     printf("tfs table with %d columns \n",tfstab->num_cols);
     printf("tfs table with %d rows    \n",tfstab->curr    );
@@ -416,9 +416,9 @@ sdds_writet_sel(char *filename, struct table *tfstab)
 
     for(j1=0; j1<col->curr; j1++) {
        /* define data arrays from TFS columns*/
-       if (SDDS_DefineArray(&SDDS_table, tfstab->columns->names[col->i[j1]], 
-                            NULL, NULL, NULL, NULL, 
-                            sdds_tfs_types[tfstab->columns->inform[col->i[j1]]], 0, 1, NULL)<0)   { 
+       if (SDDS_DefineArray(&SDDS_table, tfstab->columns->names[col->i[j1]],
+                            NULL, NULL, NULL, NULL,
+                            sdds_tfs_types[tfstab->columns->inform[col->i[j1]]], 0, 1, NULL)<0)   {
          SDDS_PrintErrors(stderr, SDDS_VERBOSE_PrintErrors);
          exit(1);
        }
@@ -469,7 +469,7 @@ sdds_writet_sel(char *filename, struct table *tfstab)
             pl[j2] = da1[pos[j1]][j2];
             if (get_option("debug")) printf(" %ld\n",pl[j2]);
           }
-          if (!SDDS_SetArrayVararg(&SDDS_table, tfstab->columns->names[col->i[j1]], 
+          if (!SDDS_SetArrayVararg(&SDDS_table, tfstab->columns->names[col->i[j1]],
                SDDS_POINTER_ARRAY, pl,tfstab->curr)) {
                SDDS_PrintErrors(stderr, SDDS_VERBOSE_PrintErrors);
                exit(1);
@@ -486,7 +486,7 @@ sdds_writet_sel(char *filename, struct table *tfstab)
             }
             /* for row->i[j2] == 1  ==> row is selected, not implemented */
           }
-          if (!SDDS_SetArrayVararg(&SDDS_table, tfstab->columns->names[col->i[j1]], 
+          if (!SDDS_SetArrayVararg(&SDDS_table, tfstab->columns->names[col->i[j1]],
                SDDS_POINTER_ARRAY, &da1[pos[j1]][0],tfstab->curr)) {
                SDDS_PrintErrors(stderr, SDDS_VERBOSE_PrintErrors);
                exit(1);
@@ -497,7 +497,7 @@ sdds_writet_sel(char *filename, struct table *tfstab)
           for(j2=0;j2<tfstab->curr; j2++) {
             if (get_option("debug")) printf(" %s\n",tfstab->s_cols[pos[j1]][j2]);
           }
-          if (!SDDS_SetArrayVararg(&SDDS_table, tfstab->columns->names[col->i[j1]], 
+          if (!SDDS_SetArrayVararg(&SDDS_table, tfstab->columns->names[col->i[j1]],
                SDDS_POINTER_ARRAY, &sa1[pos[j1]][0],tfstab->curr)) {
                SDDS_PrintErrors(stderr, SDDS_VERBOSE_PrintErrors);
                exit(1);
@@ -514,7 +514,7 @@ sdds_writet_sel(char *filename, struct table *tfstab)
       SDDS_PrintErrors(stderr, SDDS_VERBOSE_PrintErrors);
       exit(1);
     }
-    
+
   return(0);
 }
 
@@ -531,8 +531,8 @@ set_selected_rows_tab(struct table* t, struct command_list* select, struct comma
     {
       for (j = 0; j < t->curr; j++)
       {
-        if (t->row_out->i[j] == 0) t->row_out->i[j]
-                                     = pass_select_el(t->p_nodes[j]->p_elem, select->commands[i]);
+        if (t->row_out->i[j] == 0)
+          t->row_out->i[j] = pass_select_el(t->p_nodes[j]->p_elem, select->commands[i]);
         if (t->row_out->i[j] == 1) n++;
       }
     }
@@ -543,8 +543,8 @@ set_selected_rows_tab(struct table* t, struct command_list* select, struct comma
     {
       for (j = 0; j < t->curr; j++)
       {
-        if (t->row_out->i[j] == 1) t->row_out->i[j]
-                                     = 1 - pass_select(t->s_cols[0][j], deselect->commands[i]);
+        if (t->row_out->i[j] == 1)
+          t->row_out->i[j] = 1 - pass_select(t->s_cols[0][j], deselect->commands[i]);
         if (t->row_out->i[j] == 1) n++;
       }
     }
@@ -575,7 +575,7 @@ sel_table(char* tname, struct table* t)
     set_selected_rows_tab(t, scl, dscl);
   }
 }
-                                                                                                          
+
 static int
 sdds_ior(struct in_cmd* cmd)
 {
@@ -591,16 +591,16 @@ sdds_ior(struct in_cmd* cmd)
     }
     printf("access SDDS table: %s %s\n",sdds_table_file,tfs_table_name);
     i = sdds_readt(sdds_table_file,tfs_table_name);
-    return(i);
+    return i;
 }
 
-static int 
+static int
 sdds_iow(struct in_cmd* cmd)
 {
-    char *sdds_table_file;
+    char *sdds_table_file=0;
     char *tfs_table_name;
     struct table *tfs_table=0;
-    int   i, pos;
+    int i;
     if((sdds_table_file = command_par_string("file",cmd->clone)) == NULL) {
          fatal_error("No file name to write SDDS table ","\n");
     }
@@ -611,10 +611,10 @@ sdds_iow(struct in_cmd* cmd)
     mycpy(c_dum->c, tfs_table_name);
     tfs_table = find_table(c_dum->c);
 
-    printf("create SDDS table: %s %d\n",sdds_table_file,pos);
+    printf("create SDDS table: %s\n",sdds_table_file);
     sel_table(tfs_table_name, tfs_table);
     i = sdds_writet_sel(sdds_table_file,tfs_table);
-    return(i);
+    return i;
 }
 
 // public interface
