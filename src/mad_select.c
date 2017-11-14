MethodicalAcceleratorDesign/MAD-X

commit 1322870e65558e24ac3b24f81d350d79e4bca146

r6260: Deduplicate code for get_select_ranges_ex
For iterating nodes that match a SELECT/MATCH condition.

Single source of truth → more consistency!

The same implementation will also be used later for iterating nodes that
match the SELECT condition for interpolation.

Note that this requires to keep track of `match_sequs->name_list`.

Squash commit of: r6250 r6259 r6260

src/mad_select.c
@@ -1,5 +1,80 @@
 #include "madx.h"
 
+// TG: start_iter_select/fetch_node_select can be used to replace/implement
+// get_select_ex_ranges and the same pattern in a few other places.
+struct select_iter
+{
+  int i_seq, full;
+  struct command* cmd;
+  struct sequence* sequ;
+  struct sequence_list* sequs;
+  const char* range;
+  struct node *node, *range_end;
+};
+
+struct select_iter*
+start_iter_select(struct command* cmd, struct sequence_list* sequs, struct sequence* sequ)
+{
+  const char* name;
+
+  if (!sequs)
+    sequs = sequences;
+
+  if (sequs && !sequ && (name = command_par_string("sequence", cmd))) {
+    sequ = find_sequence(name, sequs);
+    sequs = NULL;
+    if (!sequ) {
+      warning("unknown sequence, skipped select: ", name);
+      return NULL;
+    }
+  }
+
+  struct select_iter* it = mycalloc("start_iter_select", 1, sizeof(struct select_iter));
+  it->cmd = cmd;
+  it->sequ = sequ ? sequ : sequs->sequs[0];
+  it->full = log_val("full", cmd);    // `full` means "unfiltered"
+  it->sequs = sequs;
+  it->range = it->full ? NULL : command_par_string("range", cmd);
+
+  return it;
+}
+
+int
+fetch_node_select(struct select_iter* it, struct node** node, struct sequence** seq)
+{
+  struct node* nodes[2];
+
+  if (!it)
+    return 0;
+
+  for (;;) {
+    if (it->node) {
+      it->node = it->node == it->range_end ? NULL : it->node->next;
+    }
+    else if (it->range) {
+      if (get_ex_range(it->range, it->sequ, nodes)) {
+        it->node = nodes[0];
+        it->range_end = nodes[1];
+      }
+    }
+    else {
+      it->node = it->sequ->ex_start;
+      it->range_end = it->sequ->ex_end;
+    }
+
+    if (!it->node && it->sequs && it->i_seq != it->sequs->curr) {
+      it->sequ = it->sequs->sequs[++it->i_seq];
+      continue;
+    }
+
+    if (!it->node || it->full || pass_select(it->node->p_elem->name, it->cmd)) {
+      if (seq) *seq = it->sequ;
+      if (node) *node = it->node;
+      return it->node != NULL;
+    }
+  }
+}
+
 static int
 get_select_ex_ranges(struct sequence* sequ, struct command_list* select, struct node_list* s_ranges)
   /* makes a list of nodes of an expanded sequence that pass the range
@@ -7,44 +82,17 @@ get_select_ex_ranges(struct sequence* sequ, struct command_list* select, struct 
 {
   /*returns 0 if invalid sequence pointer
     1 if nodes in s_ranges (including 0) */
-  struct name_list* nl;
-  struct command* cd;
-  struct command_parameter_list* pl;
-  char* name;
-  int full = 0, i, pos; // k, // not used
   struct node* c_node;
-  struct node* nodes[2];
   if (sequ == NULL) return 0;
   s_ranges->curr = 0;
   s_ranges->list->curr = 0;
-  for (i = 0; i < select->curr; i++)
+  for (int i = 0; i < select->curr; i++)
   {
-    cd = select->commands[i];
-    nl = cd->par_names;
-    pl = cd->par;
-    pos = name_list_pos("full", nl);
-    if ((pos = name_list_pos("full", nl)) > -1 && nl->inform[pos]
-        && command_par_value("full", cd) != zero) full = 1;
-    if (full == 0 && (pos = name_list_pos("range", nl)) > -1
-        && nl->inform[pos])
-    {
-      name = pl->parameters[pos]->string;
-      if (get_ex_range(name, sequ, nodes) == 0) return 0; // (k = not used
+    struct select_iter* it = start_iter_select(select->commands[i], NULL, sequ);
+    while (fetch_node_select(it, &c_node, NULL)) {
+      add_to_node_list(c_node, 0, s_ranges);
     }
-    else
-    {
-      if ((nodes[0] = sequ->ex_start) == NULL ||
-          (nodes[1] = sequ->ex_end) == NULL) return 0;
-    }
-    c_node = nodes[0];
-    while (c_node != NULL)
-    {
-      if (full != 0 || pass_select(c_node->p_elem->name, cd) != 0)
-        add_to_node_list(c_node, 0, s_ranges);
-      if (c_node == nodes[1]) break;
-      c_node = c_node->next;
-    }
-    if (full != 0) break;
+    if (it->full) break;
   }
   return 1;
 }

src/mad_match.c
@@ -316,14 +316,7 @@ match_cell(struct in_cmd* cmd)
 static void
 match_constraint(struct in_cmd* cmd)
 {
-  char* name;
-  struct command_parameter* cp;
-  struct name_list* nl = cmd->clone->par_names;
-  struct command_parameter_list* pl = cmd->clone->par;
-  struct sequence* sequ;
-  struct node* nodes[2];
   struct node* c_node;
-  int pos, n, low, up; // , k // not used
 
   if(match_is_on==2)
   {
@@ -335,45 +328,12 @@ match_constraint(struct in_cmd* cmd)
   }
   else
   { /* old match */
-    pos = name_list_pos("sequence", nl);
-    if((pos>=0)?nl->inform[pos]:0) { /* sequence specified */
-      cp = cmd->clone->par->parameters[pos];
-      for (n = 0; n < match_sequs->curr; n++)
-        if (strcmp(cp->string, match_sequs->sequs[n]->name) == 0)
-          break;
-
-      if (n == match_sequs->curr) {
-        warning(cp->string," :sequence not selected by MATCH, skipped");
-        return;
-      }
-
-      low = up = n;
-    }
-    else {
-      low = 0; up = match_sequs->curr - 1;
-    }
-
-    for (n = low; n <= up; n++) {
-      sequ = match_sequs->sequs[n];
-      pos = name_list_pos("range", nl);
-      if ((pos>=0)?nl->inform[pos]:0) { /* parameter has been read */
-        name = pl->parameters[pos]->string;
-        if (get_ex_range(name, sequ, nodes) == 0)  return; // (k = not used
-      }
-      else {
-        nodes[0] = sequ->ex_start; nodes[1] = sequ->ex_end;
-      }
-      c_node = nodes[0];
-      comm_constraints->curr=0;
-      fill_constraint_list(1, cmd->clone, comm_constraints);
-
-      while (c_node)
-      {
-        if (pass_select(c_node->p_elem->name, cmd->clone) != 0)
-          update_node_constraints(c_node, comm_constraints);
-        if (c_node == nodes[1]) break;
-        c_node = c_node->next;
-      }
+    // TODO: use all sequences!
+    comm_constraints->curr=0;
+    fill_constraint_list(1, cmd->clone, comm_constraints);
+    struct select_iter* it = start_iter_select(cmd->clone, match_sequs, NULL);
+    while (fetch_node_select(it, &c_node, NULL)) {
+      update_node_constraints(c_node, comm_constraints);
     }
   }
 }
@@ -485,6 +445,7 @@ match_end(struct in_cmd* cmd)
   match_is_on = 0;
   set_option("match_is_on", &izero);
   match_sequs->curr = 0;
+  match_sequs->list->curr = 0;
   current_call_lim = 0;
   current_calls = 0;
   total_const = 0;
@@ -625,6 +586,7 @@ match_match(struct in_cmd* cmd)
           return;
         }
         match_sequs->sequs[match_sequs->curr++] = sequ;
+        add_to_name_list(sequ->name, 0, match_sequs->list);
       }
     }
     if (match_sequs->curr == 0)
@@ -634,7 +596,10 @@ match_match(struct in_cmd* cmd)
       return;
     }
   }
-  else match_sequs->sequs[match_sequs->curr++] = current_sequ;
+  else {
+    match_sequs->sequs[match_sequs->curr++] = current_sequ;
+    add_to_name_list(current_sequ->name, 0, match_sequs->list);
+  }
   pos = name_list_pos("vlength", nl);
   if((pos>=0)?nl->inform[pos]:0) i = pl->parameters[pos]->double_value;
   else i = 0;

src/mad_select.h
@@ -9,6 +9,9 @@ struct table;
 struct sequence;
 struct command;
 struct command_list;
+struct select_iter;
+struct sequence;
+struct sequence_list;
 
 // interface
 
@@ -25,6 +28,9 @@ void  set_selected_errors(void);
 void  set_selected_columns(struct table*, struct command_list*);
 void  set_range(char* range, struct sequence*);
 void  set_sector(void);
+
+struct select_iter* start_iter_select(struct command*, struct sequence_list*, struct sequence*);
+int                 fetch_node_select(struct select_iter*, struct node**, struct sequence**);
 
 #endif // MAD_SELECT_H
 

src/mad_seq.c
@@ -2154,4 +2154,10 @@ set_enable(const char* type, struct in_cmd* cmd)
   return count;
 }
 
-
+struct sequence* find_sequence(const char* name, struct sequence_list* sequs)
+{
+  if (!sequs)
+    sequs = sequences;
+  int pos = name_list_pos(name, sequs->list);
+  return pos >= 0 ? sequs->sequs[pos] : NULL;
+}

src/mad_seq.h
@@ -69,6 +69,7 @@ struct node*     new_sequ_node(struct sequence*, int occ_cnt);
 struct sequence* new_sequence(const char* name, int ref);
 struct sequence* delete_sequence(struct sequence*);
 struct sequence_list* new_sequence_list(int length);
+struct sequence* find_sequence(const char* name, struct sequence_list*);
 
 void    use_sequ(struct in_cmd*);
 void    remove_from_sequ_list(struct sequence*, struct sequence_list*);

commit 5e0eeadf227d4c1c7269498a69e816b48572e303

Fix fetch_node_select crash when used with multiple sequences
Fixes #501

Bug introduced in 1322870e65558e24ac3b24f81d350d79e4bca146.

src/mad_select.c
@@ -62,7 +62,7 @@ fetch_node_select(struct select_iter* it, struct node** node, struct sequence** 
       it->range_end = it->sequ->ex_end;
     }
 
-    if (!it->node && it->sequs && it->i_seq != it->sequs->curr) {
+    if (!it->node && it->sequs && 1+it->i_seq < it->sequs->curr) {
       it->sequ = it->sequs->sequs[++it->i_seq];
       continue;
     }