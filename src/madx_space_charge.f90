899baa4833d2a1c13aebc5d12b2f83db2e98bf5e
Andrea Latina <andrea.latina@cern.ch>
2018-05-11 12:23:17 +0200

    Synch with master, and add first madx_space_charge.f90

---
+++ src/madx_space_charge.f90
@@ -0,0 +1,15 @@
+module SpaceCharge
+
+  implicit none
+  private
+
+  logical :: sc_chrom_fix
+
+  public :: sc_chrom_fix
+
+contains
+  
+  subroutine Init()
+  end subroutine Init
+
+end module SpaceCharge
