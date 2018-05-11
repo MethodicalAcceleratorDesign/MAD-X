8ccf17ef3c393e4362fa10c61a4cebcb5071351d
Andrea Latina <andrea.latina@cern.ch>
2018-05-11 18:05:24 +0200

    Just started...

--- src/madx_space_charge.f90
+++ src/madx_space_charge.f90
@@ -9,7 +9,12 @@ module SpaceCharge
 
 contains
   
-  subroutine Init()
-  end subroutine Init
+  subroutine Init_SC()
+!!$    exit_loss_turn = get_option('exit_loss_turn ') .ne. 0
+!!$    bb_sxy_update = get_option('bb_sxy_update ') .ne. 0
+!!$    checkpnt_restart = get_value('run ', 'checkpnt_restart ') .ne. zero
+!!$    emittance_update = get_option('emittance_update ') .ne. 0
+!!$    virgin_state = get_value('run ', 'virgin_state ') .ne. zero
+  end subroutine Init_SC
 
 end module SpaceCharge
