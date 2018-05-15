f3a1b92948e7f91ff5d49259f323840ecbca37a9
tobias <tobias@pcbe15807.dyndns.cern.ch>
2018-05-15 12:57:07 +0200

    Implements the new rows. The tests that changed are also included.

--- src/mad_gcst.c
+++ src/mad_gcst.c
@@ -190,7 +190,7 @@ const char* const sxf_table_names[] =
 const int twiss_opt_end = 33; /* last column filled by twiss module */
 const int twiss_mult_end = 78; /* last multipole column filled
                             by complete_twiss_table */
-const int twiss_fill_end = 97; /* last standard column filled
+const int twiss_fill_end = 102; /* last standard column filled
                             by complete_twiss_table */
 /*== increased twiss_fill_end from 96 to 97 to accomodate for v_pos */
 
@@ -219,6 +219,7 @@ const int twiss_table_types[] =
   2, 2, 2, 2,
   1, 1, 2,
   2, /* v_pos */
+  2, 2, 2, 2, 2,
   2, 3,
   2, 2, 2, 2, 2, 2,
   2, 2, 2, 2, 2, 2,
@@ -288,6 +289,7 @@ const char* const twiss_table_cols[] =
   "slot_id","assembly_id","mech_sep",
   /*== dealt with the new property v_pos as for mech_sep */
   "v_pos",
+  "bbcharge","xma", "yma", "sigx", "sigy",
   /*==*/
   "lrad","parent",
   "re11", "re12", "re13", "re14", "re15", "re16",
--- src/mad_twiss.c
+++ src/mad_twiss.c
@@ -599,6 +599,12 @@ complete_twiss_table(struct table* t)
     else if (strcmp(tmp, "lrad") == 0) val =  el_par_value(tmp, c_node->p_elem);
     else if (strcmp(tmp, "h1") == 0 &&
              strcmp(c_node->base_name, "dipedge") == 0) val = el_par_value("h", c_node->p_elem);
+    else if (strcmp(tmp, "bbcharge") == 0) val =  el_par_value("charge", c_node->p_elem);
+    else if (strcmp(tmp, "xma") == 0) val =  el_par_value(tmp, c_node->p_elem);
+    else if (strcmp(tmp, "yma") == 0) val =  el_par_value(tmp, c_node->p_elem);
+    else if (strcmp(tmp, "sigx") == 0) val =  el_par_value(tmp, c_node->p_elem);
+    else if (strcmp(tmp, "sigy") == 0) val =  el_par_value(tmp, c_node->p_elem); 
+
     else if(mult)
     {
       if(j<=twiss_mult_end)
@@ -636,6 +642,8 @@ complete_twiss_table(struct table* t)
             && strcmp(tmp, "freq") && strcmp(tmp, "harmon")) val *= el;
       }
     }
+
+
     t->d_cols[j][i] = val;
   }
 }
