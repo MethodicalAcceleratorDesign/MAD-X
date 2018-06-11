2252e78ce0e73bba7adb6e0af458ee0b8b941c59
Laurent DENIAU <laurent.deniau@gmail.com>
2018-06-11 14:51:35 +0200

    finalize

--- src/mad_dbg.c
+++ src/mad_dbg.c
@@ -1,9 +1,11 @@
+#include <stdio.h>
 #include "mad_dbg.h"
 
 /* special assert that loops to let the debugger to catch the process through
    its PID and perform a backtrace. */
 
-static volatile int __assert_foo;
+static volatile int __assert_foo = 0;
+
 void __assert_fail(const char *assertion, const char *file, int line,
                    const char *function)
 {
