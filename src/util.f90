d521460ac502b3f2cfac89ee5b10a1102fd62fa6
tobias <tobias@pcbe15807.dyndns.cern.ch>
2019-08-15 16:19:01 +0200

    Was a misstake in util..

--- src/util.f90
+++ src/util.f90
@@ -128,7 +128,22 @@ module aperture_enums
   integer, parameter :: ap_custom = 8
 
 end module aperture_enums
-
+module track_enums
+  implicit none
+  public
+  integer, parameter :: enum_other_bv = 1
+  integer, parameter :: enum_lrad = 2
+  integer, parameter :: enum_noise = 3
+  integer, parameter :: enum_angle = 4
+  integer, parameter :: enum_time_var = 5
+  integer, parameter :: enum_sinkick = 6
+  integer, parameter :: enum_kick = 7
+  integer, parameter :: enum_chkick = 8
+  integer, parameter :: enum_cvkick = 9
+  integer, parameter :: enum_hkick = 10 
+  integer, parameter :: enum_vkick = 11  
+  integer, parameter :: total_enums = 11
+end module track_enums
 module Inf_NaN_Detection
 
   !!     Inf_NaN_Detection module
